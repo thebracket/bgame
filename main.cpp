27e28c88303a96336b6ca10746a137c17d1c6002
Herbert Wolverson <herberticus@gmail.com>
2015-10-03 10:39:06 -0500

    tmp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <memory>
 #include "engine/engine.h"
 #include "test_mode.h"
+#include "game/loading_screen.h"
 
 using std::cout;
 using std::make_unique;
 
 int main()
 {
-     engine::init(engine::SDL2);
-     engine::main_loop ( make_unique<test_mode>() );
+     engine::init(engine::NCURSES);
+     engine::main_loop ( make_unique<loading_screen>() );
 }
 
