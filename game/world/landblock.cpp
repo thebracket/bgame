a3deb51256b155c39aa7bc741235417bf391c073
Herbert Wolverson <herberticus@gmail.com>
2015-10-07 12:00:50 -0500

    Add a random (nonsensical) set of tile variations, so that we can tell when the player icon moves.

--- game/world/landblock.cpp
+++ game/world/landblock.cpp
@@ -1,6 +1,18 @@
 #include "landblock.h"
+#include "../../engine/rng.h"
 
 land_block::land_block()
 {
   tiles.resize(tiles_per_landblock);
+  for (tile &t : tiles) {
+    int symbol = engine::roll_dice(1,6);
+    switch (symbol) {
+      case 1 : t.display = '.'; break;
+      case 2 : t.display = '#'; break;
+      case 3 : t.display = '`'; break;
+      case 4 : t.display = '\''; break;
+      case 5 : t.display = '"'; break;
+      case 6 : t.display = '%'; break;
+    }
+  }
 }
