781cbf9f3c852cf03d4e3fc23505e73b8cdddee4
thebracket <herberticus@gmail.com>
2015-10-17 22:50:14 -0500

    Work In Progress: Render list added to the world, ready for the renderables system.

--- game/world/world.cpp
+++ game/world/world.cpp
@@ -7,6 +7,7 @@ namespace world {
 array<unique_ptr<land_block>, world_size> world;
 console log;
 land_block * current_region;
+map<int, engine::vterm::screen_character> entity_render_list;
 
 int camera_handle;
 }
