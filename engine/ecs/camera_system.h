cdc3ccce8f42cfaecdf1ef08ff1d2bc85ea95a1e
thebracket <herberticus@gmail.com>
2015-10-21 20:03:12 -0500

    Camera system into its own CPP file.

--- engine/ecs/camera_system.h
+++ engine/ecs/camera_system.h
@@ -10,27 +10,7 @@ namespace engine {
 namespace ecs {
 
 class camera_system : public base_system {
-    virtual void tick(const double &duration_ms) {
-        const entity * camera = get_entity_by_handle(world::camera_handle);
-        position_component * camera_pos = get_component_by_handle<position_component>(camera->find_component_by_type(ecs::position));
-
-        if ( command::is_key_down ( command::UP ) ) {
-            camera_pos->y--;
-            if (camera_pos->y < 0) camera_pos->y = 0;
-        }
-        if ( command::is_key_down ( command::DOWN ) ) {
-            camera_pos->y++;
-            if (camera_pos->y > landblock_height) camera_pos->y = landblock_height;
-        }
-        if ( command::is_key_down ( command::LEFT ) ) {
-            camera_pos->x--;
-            if (camera_pos->x < 0) camera_pos->x = 0;
-        }
-        if ( command::is_key_down ( command::RIGHT ) ) {
-            camera_pos->x++;
-            if (camera_pos->x > landblock_width) camera_pos->x = landblock_width;
-        }
-    }
+    virtual void tick(const double &duration_ms);
 };
 
 }
