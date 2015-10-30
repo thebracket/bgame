5047728f90564f7b1a0708f9a2e4c4521aef68d1
Herbert Wolverson <herberticus@gmail.com>
2015-10-30 08:25:22 -0500

    Remove the last mention of "world" from the engine section!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "game/loading_screen.h"
 #include "config.h"
 #include "engine/globals.h"
+#include "game/world/world.h"
 
 #include "game/components/components.h"
 
@@ -14,7 +15,7 @@ using std::make_unique;
 int main()
 {
      GAME engine;
-     GAME_ECS ecs(component_factory);
+     GAME_ECS ecs(component_factory, world::load_world_constants, world::save_world_constants);
      engine::globals::ecs = &ecs;
      engine.init();
      engine.main_loop( make_unique<loading_screen>() );
