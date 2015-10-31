64b5c1f37d7310211c012b07f992dc85ad5b6cbb
thebracket <herberticus@gmail.com>
2015-10-31 15:12:46 -0500

    ECS is now header-only.

--- engine/entity.h
+++ engine/entity.h
@@ -9,10 +9,23 @@ namespace engine {
 class entity {
 public:
     int handle;
-    void save(fstream &lbfile);
-    void load(fstream &lbfile);
+    
+    void load ( fstream& lbfile )
+    {
+	lbfile.read ( reinterpret_cast<char *> ( &handle ), sizeof ( handle ) );
+    }
+
+    void save ( fstream& lbfile )
+    {
+	lbfile.write ( reinterpret_cast<const char *> ( &handle ), sizeof ( handle ) );
+    }
 };
 
-entity construct_entity_from_file(fstream &lbfile);
+inline entity construct_entity_from_file ( fstream& lbfile )
+{
+    entity e;
+    e.load(lbfile);
+    return e;
+}
 
 }
