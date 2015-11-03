0fb6ca4912472f70f4d69d7e6a1b4d4c489e131f
Herbert Wolverson <herberticus@gmail.com>
2015-11-03 15:05:13 -0600

    Settlers now have a direction that they are facing, and their view is adjusted by which way they are looking.

--- game/systems/viewshed_system.cpp
+++ game/systems/viewshed_system.cpp
@@ -34,6 +34,30 @@ void viewshed_system::reset_visibility() {
     std::fill(world::current_region->visible.begin(), world::current_region->visible.end(), false);
 }
 
+
+bool viewshed_system::is_facing_this_way ( const position_component* pos, const double& angle )
+{  
+    //constexpr double ZERO = 0;
+    constexpr double FORTY_FIVE = 45.0 * DEGRAD;
+    //constexpr double NINETY = 90.0 * DEGRAD;
+    constexpr double ONE_THIRTY_FIVE = 135.0 * DEGRAD;
+    //constexpr double ONE_EIGHTY = 180.0 * DEGRAD;
+    constexpr double TWO_TWENTY_FIVE = 225.0 * DEGRAD;
+    //constexpr double TWO_SEVENTY = 270.0 * DEGRAD;
+    constexpr double THREE_FIFTEEN = 315.0 * DEGRAD;
+    //constexpr double THREE_SIXTY = 360.0 * DEGRAD;
+
+    if (pos->facing == OMNI) return true;
+    
+    if (pos->facing == NORTH and (angle > TWO_TWENTY_FIVE and angle < THREE_FIFTEEN)) return true;    
+    if (pos->facing == SOUTH and (angle > FORTY_FIVE and angle < ONE_THIRTY_FIVE)) return true;
+    
+    if (pos->facing == EAST and (angle > THREE_FIFTEEN or angle < FORTY_FIVE)) return true;
+    if (pos->facing == WEST and (angle > ONE_THIRTY_FIVE and angle < TWO_TWENTY_FIVE)) return true;
+    
+    return false;
+}
+
 void viewshed_system::scan_radius_for_visibility(viewshed_component * view, const position_component * pos) {
     constexpr double sweep_degrees_radians = 1 * DEGRAD;
     constexpr double three_sixy_degrees_radians = 360 * DEGRAD;
@@ -50,7 +74,9 @@ void viewshed_system::scan_radius_for_visibility(viewshed_component * view, cons
 
     // Sweep around
     for ( double angle=0; angle<three_sixy_degrees_radians; angle+=sweep_degrees_radians ) {
-        pair<int,int> destination = project_angle ( x, y, radius, angle );
+	double this_radius = radius;
+	if (!is_facing_this_way(pos, angle)) this_radius = radius/4;
+        pair<int,int> destination = project_angle ( x, y, this_radius, angle );
         bool blocked = false;
         line_func ( x, y, destination.first, destination.second, [&blocked,view,level_band] (int tx, int ty) {
             if (tx < 0 or tx > landblock_width or ty < 0 or ty > landblock_height) return;
@@ -83,12 +109,15 @@ void viewshed_system::scan_radius_penetrating(viewshed_component * view, const p
     // You can always see yourself
     const int x = pos->x;
     const int y = pos->y;
-    const int radius = view->scanner_range;
+    const double radius = view->scanner_range;
     view->last_visibility.push_back(world::current_region->idx(x, y));
 
     // Sweep around
     for ( double angle=0; angle<three_sixy_degrees_radians; angle+=sweep_degrees_radians ) {
-        pair<int,int> destination = project_angle ( x, y, radius, angle );
+	double this_radius = radius;
+	if (!is_facing_this_way(pos, angle) == true) this_radius = 2.0;
+	
+        pair<int,int> destination = project_angle ( x, y, this_radius, angle );
         line_func ( x, y, destination.first, destination.second, [view] (int tx, int ty) {
             const int index = world::current_region->idx(tx,ty);
             if (tx >=0 and tx < landblock_width and ty>=0 and ty<landblock_height) {
