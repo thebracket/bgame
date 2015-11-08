d3d133216e652028dcad58781b576f534635c511
thebracket <herberticus@gmail.com>
2015-11-08 09:53:32 -0600

    Move loading the font texture into the new image resource class.

--- engine/sdl2_resources.h
+++ engine/sdl2_resources.h
@@ -4,20 +4,21 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <iostream>
 
 namespace engine {
 
 struct texture_resource {
     texture_resource(SDL_Renderer * renderer, const std::string &fn, const std::string &tag) : filename(fn), name(tag) 
     {
-	SDL_Surface * tmp_surface = IMG_Load ( "../assets/terminal8x8_palette2.png" );
+	SDL_Surface * tmp_surface = IMG_Load ( fn.c_str() );
 	if ( tmp_surface == NULL ) throw 104;
 	image = SDL_CreateTextureFromSurface ( renderer, tmp_surface );
 	SDL_FreeSurface ( tmp_surface );
 	initialized = true;
     }
-    
-    ~texture_resource() {
+        
+    void clear() {
 	if (initialized) SDL_DestroyTexture(image);
     }
   
@@ -29,10 +30,20 @@ struct texture_resource {
 
 struct sdl_resource_manager {
     int load_image(SDL_Renderer * renderer, const std::string &filename, const std::string &tag) {
+	int image_flags = IMG_INIT_PNG;
+	if ( ! ( IMG_Init ( image_flags ) & image_flags ) ) throw 104;
+      
 	const int index = textures.size();
-	textures.push_back( texture_resource( renderer, filename, tag ) );
+	texture_resource res = texture_resource( renderer, filename, tag );
+	textures.push_back( std::move( res ) );
 	texture_index[tag] = index;
 	return index;
+    }
+    
+    void clear() {
+	texture_index.clear();
+	for (texture_resource &t : textures) t.clear();
+	textures.clear();
     }
     
     SDL_Texture * get_texture_by_id(const int &id) { return textures[id].image; }
