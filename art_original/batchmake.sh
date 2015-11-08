c74f79f7211bb7105d530a9a6d0a1dea5aaead01
thebracket <herberticus@gmail.com>
2015-11-08 15:26:34 -0600

    Add a basic set of tiles, and a quick spritesheet maker (reads GIMP layers)

---
+++ art_original/batchmake.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MAX_LAYER=4
+MIN_LAYER=0
+INFILE="32x32.xcf"
+
+mkdir tmp
+for (( i=MIN_LAYER; i<=MAX_LAYER; i++ ))
+do
+	OUTFILE=tmp/img$i.png
+	CONVERSION="convert $INFILE[$i] $OUTFILE"
+	$CONVERSION
+done
+montage -background transparent -geometry 32x32\>+0+0  tmp/*.png spritesheet.png
+rm tmp/*.png
+rmdir tmp
+
