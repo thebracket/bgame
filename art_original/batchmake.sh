e3e2ff36543bae23ee884b9cc3ee84266509e446
thebracket <herberticus@gmail.com>
2015-11-10 00:09:14 -0600

    First try at graphic mode lighting; not right, but committing before bed!

--- art_original/batchmake.sh
+++ art_original/batchmake.sh
@@ -76,6 +76,9 @@ convert 16x16.xcf[26] tmp/045.png
 convert 16x16.xcf[27] tmp/046.png
 convert 16x16.xcf[28] tmp/047.png
 convert 16x16.xcf[29] tmp/048.png
+convert 16x16.xcf[30] tmp/049.png
+convert 16x16.xcf[31] tmp/050.png
+convert 16x16.xcf[32] tmp/051.png
 
 montage -background transparent -geometry 16x16\>+0+0 tmp/*.png $RESULT
 rm tmp/*
