e6d782e10160f25191686379685d600926058968
thebracket <herberticus@gmail.com>
2015-11-14 21:26:21 -0600

    Clean up the tool-tip code.

---
+++ game/colors.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace render {
+
+constexpr SDL_Color sdl_green { 64,255,64,0 };
+constexpr SDL_Color sdl_cyan { 0,255,255,0 };
+constexpr SDL_Color sdl_white { 255,255,255,0 };
+  
+}
