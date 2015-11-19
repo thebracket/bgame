3f869a0426ca24c56a42e808426906f81c3d56b3
Herbert Wolverson <herbert@iMac.local>
2015-11-19 15:22:30 -0600

    Slightly more flesh on the job system placeholder skeleton.

---
+++ game/tasks/help_wanted.cpp
@@ -0,0 +1,20 @@
+#include "help_wanted.h"
+
+namespace ai {
+
+namespace ai_detail {
+int next_job_id = 1;
+}
+
+int get_next_job_id()
+{
+     ++ai_detail::next_job_id;
+     return ai_detail::next_job_id;
+}
+
+unordered_map<int, job> jobs_board;
+
+void save_help_wanted ( fstream& lbfile ) {};
+void load_help_wanted ( fstream& lbfile ) {};
+
+}
--- game/tasks/help_wanted.h
+++ game/tasks/help_wanted.h
@@ -1,27 +1,41 @@
 #pragma once
 
 #include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
 
 using std::fstream;
+using std::vector;
+using std::string;
+using std::unordered_map;
 
 namespace ai {
-  
-  struct job {
-    // Job ID
-    // Job Type
-    // List of Steps
-    // Current Step
-    // Assigned To
-  };
-  
-  struct job_step {
-    // Step Type
-    // Location
-    // Skill if any
-  };
-  
-  void save_help_wanted( fstream& lbfile ) {};
-  void load_help_wanted( fstream& lbfile ) {};
-  
-  // map of jobs
+
+enum job_type_t { BUILD_STRUCTURE };
+enum job_step_t { MOVE_TO, PICK_UP, CONSTRUCT_WITH_SKILL, CONVERT_PLACEHOLDER_STRUCTURE };
+
+int get_next_job_id();
+
+struct job_step {
+     job_step_t type;
+     int target_x = 0;
+     int target_y = 0;
+     bool requires_skill = false;
+     string skill_name;
+     int placeholder_structure_id;
+};
+
+struct job {
+     job_type_t type;
+     int job_id;
+     vector<job_step> steps;
+     int current_step = 0;
+     int assigned_to = 0;
+};
+
+extern unordered_map<int, job> jobs_board;
+
+void save_help_wanted ( fstream& lbfile );
+void load_help_wanted ( fstream& lbfile );
 }
