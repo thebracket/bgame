0ae438cb12fbcff71bff5e47856835f69994c6e2
Herbert Wolverson <herberticus@gmail.com>
2015-11-19 15:22:30 -0600

    Slightly more flesh on the job system placeholder skeleton.

--- game/systems/cordex_ai_system.cpp
+++ game/systems/cordex_ai_system.cpp
@@ -7,9 +7,9 @@ void cordex_ai_system::tick ( const double& duration_ms )
     // Handle new build orders
     vector<build_order_message> * orders = game_engine->messaging->get_messages_by_type< build_order_message >();
     for (const build_order_message &msg : *orders) {
-	msg.deleted = true;
 	std::cout << "Logged an order to build [" << msg.structure_name << "], at {" << msg.x << "," << msg.y << "}\n";
 	
+	// Create a place-holder entity for the new structure (just a renderable and a position, obtain the entity ID)
 	// Create a job
 	// Create the sub-tasks
 	// Add it to the help-wanted board
