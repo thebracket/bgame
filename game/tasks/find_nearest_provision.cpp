e902659b99f0eaea673c619b1ce53a2598356497
thebracket <herberticus@gmail.com>
2015-11-21 15:21:17 -0600

    Beds are now occupied when in use (no more dogpile of settlers).

--- game/tasks/find_nearest_provision.cpp
+++ game/tasks/find_nearest_provision.cpp
@@ -15,10 +15,15 @@ int find_nearest_provision ( const int& provision_type, const pair<int,int> &sta
      vector<provisions_component> * provisions = game_engine->ecs->find_components_by_type< provisions_component >();
      for ( const provisions_component &provision : *provisions ) {
           if ( provision.provided_resource == provision_type ) {
-               position_component * pos = game_engine->ecs->find_entity_component<position_component> ( provision.entity_id );
-               float distance = std::sqrt ( ( std::abs ( start.first - pos->x ) * std::abs ( start.first - pos->x ) ) +
-                                            std::abs ( start.second - pos->y ) * std::abs ( start.second - pos->y ) );
-               options[distance] = pos->entity_id;
+               // NOTE: Tents/beds aren't shared, so we use provides_quantity for sleep
+               // to denote occupancy. If its a type 3 (bed) then occupancy must be
+               // zero to consider it. This makes it more likely that settlers complain.
+               if ( provision.provided_resource != 3 or provision.provides_quantity==0 ) {
+                    position_component * pos = game_engine->ecs->find_entity_component<position_component> ( provision.entity_id );
+                    float distance = std::sqrt ( ( std::abs ( start.first - pos->x ) * std::abs ( start.first - pos->x ) ) +
+                                                 std::abs ( start.second - pos->y ) * std::abs ( start.second - pos->y ) );
+                    options[distance] = pos->entity_id;
+               }
           }
      }
 
