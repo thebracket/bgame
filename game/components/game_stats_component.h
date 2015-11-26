ab806821b131db84279846e558f41b7540b2011d
thebracket <herberticus@gmail.com>
2015-11-26 10:11:26 -0600

    Add component primitive for storing creatures having skills.

--- game/components/game_stats_component.h
+++ game/components/game_stats_component.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include "component_types.h"
 
+using std::vector;
 using std::string;
 
 using namespace serialization_generic;
+
+struct skill_t {
+  string skill_name;
+  char skill_level;
+};
 
 struct game_stats_component {
      game_stats_component() {}
@@ -24,7 +31,7 @@ struct game_stats_component {
      short comeliness;
      short ethics;
      int age;
-     
+     vector<skill_t> skills;
      
      void save ( fstream &lbfile ) {
           save_common_component_properties<game_stats_component> ( lbfile, *this );
@@ -37,6 +44,11 @@ struct game_stats_component {
           save_primitive<short> ( lbfile, comeliness );
 	  save_primitive<short> ( lbfile, ethics );
 	  save_primitive<int> ( lbfile, age );
+	  save_primitive<int>( lbfile, skills.size() );
+	  for (const skill_t &skill : skills) {
+	      save_primitive<string>( lbfile, skill.skill_name );
+	      save_primitive<char>( lbfile, skill.skill_level );
+	  }
      }
 
      void load  ( fstream &lbfile ) {
@@ -50,5 +62,12 @@ struct game_stats_component {
           load_primitive<short> ( lbfile, comeliness );
 	  load_primitive<short> ( lbfile, ethics );
 	  load_primitive<int>( lbfile, age );
+	  int number_of_skills;
+	  load_primitive<int>( lbfile, number_of_skills );
+	  for (int i=0; i<number_of_skills; ++i) {
+	      skill_t new_skill;
+	      load_primitive<string>( lbfile, new_skill.skill_name );
+	      load_primitive<char>( lbfile, new_skill.skill_level );
+	  }
      }
 };
