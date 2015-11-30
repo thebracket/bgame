3e3750b01603d9dafbd2abe3287c4594ac1b935f
Herbert Wolverson <herberticus@gmail.com>
2015-11-30 13:23:42 -0600

    Rearrange the render system; more things have renderables than positions, so it makes sense to search by position first.

--- game/systems/renderable_system.cpp
+++ game/systems/renderable_system.cpp
@@ -15,17 +15,13 @@ void renderable_system::tick ( const double &duration_ms ) {
 	// Clear the visible entity system
 	std::fill( world::entity_render_list.begin(), world::entity_render_list.end(), world::render_layers_t() );
 
-	// Obtain a list of things that can be rendered
-	const vector<renderable_component> * renderables = game_engine->ecs->find_components_by_type<renderable_component> ();
-	for ( const renderable_component &render_info : *renderables ) {
-
-	    position_component * pos = game_engine->ecs->find_entity_component<position_component>(render_info.entity_id);
-
-	    if ( pos != nullptr) {
-		const int idx = world::current_region->idx ( pos->x, pos->y );
-		//world::entity_render_list[idx] = std::make_tuple(render_info.tile_idx, render_info.glyph, render_info.foreground, render_info.background);
-		world::render_info_t target{ render_info.tile_idx, render_info.glyph, render_info.foreground, render_info.background, render_info.translucent, render_info.composite, render_info.entity_id, render_info.extra_tall };
-		switch (render_info.layer) {
+	const vector<position_component> * positions = game_engine->ecs->find_components_by_type<position_component> ();
+	for ( const position_component &pos : *positions ) {
+	    renderable_component * render_info = game_engine->ecs->find_entity_component<renderable_component>( pos.entity_id );
+	    if (render_info != nullptr) {
+		const int idx = world::current_region->idx ( pos.x, pos.y );
+		world::render_info_t target{ render_info->tile_idx, render_info->glyph, render_info->foreground, render_info->background, render_info->translucent, render_info->composite, render_info->entity_id, render_info->extra_tall };
+		switch (render_info->layer) {
 		  case 0 : world::entity_render_list[idx].floor = target; break;
 		  case 1 : world::entity_render_list[idx].building = target; break;
 		  case 2 : world::entity_render_list[idx].top = target; break;
