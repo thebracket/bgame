f155ff55a3554cab99a2b50f7cec6b0ccc74bd38
Herbert Wolverson <herberticus@gmail.com>
2015-12-02 12:11:28 -0600

    Planks from sawmills, and wooden floors from planks.

--- game/systems/settler_ai_system.cpp
+++ game/systems/settler_ai_system.cpp
@@ -376,12 +376,14 @@ void do_your_job ( settler_ai_component &settler, game_stats_component * stats, 
 	  for (int i=0; i<number_of_logs; ++i) {
 	      int wood_id = raws::create_item_from_raws("Wood Logs");
 	      game_engine->ecs->add_component<position_component>( *game_engine->ecs->get_entity_by_handle( wood_id ), position_component( step.target_x, step.target_y ) );
+	      game_engine->messaging->add_message<item_changed_message> ( item_changed_message ( wood_id ) );
 	  }
 	  ++job->second.current_step;
      } break;
      case ai::CREATE_ITEM : {
 	  int wood_id = raws::create_item_from_raws(step.skill_name);
 	  game_engine->ecs->add_component<position_component>( *game_engine->ecs->get_entity_by_handle( wood_id ), position_component( step.target_x, step.target_y ) );
+	  game_engine->messaging->add_message<item_changed_message> ( item_changed_message ( wood_id ) );
 	  ++job->second.current_step;
      } break;
      }
