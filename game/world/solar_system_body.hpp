ce32ae804187f9df86b488d8f963081be405b16c
thebracket <herberticus@gmail.com>
2015-12-05 14:46:04 -0600

    Fix my naming.

--- game/world/solar_system_body.hpp
+++ game/world/solar_system_body.hpp
@@ -4,7 +4,7 @@
 #include <memory>
 
 /* Base class for planetary/other body definition. */
-struct base_solar_system_body {
+struct base_solar_system_body_t {
 };
 
 /*
@@ -15,5 +15,5 @@ struct solar_system_body_t {
     uint8_t system_idx;
     
     /* Pointer to the appropriate planetary class. */
-    std::shared_ptr<base_solar_system_body> body_def;
+    std::shared_ptr<base_solar_system_body_t> body_def;
 };
