f2e5da6dcfec58e75eb12da6d5b2ba252eb953dd
thebracket <herberticus@gmail.com>
2015-12-05 14:32:01 -0600

    Start serialization for the new universe.

---
+++ game/world/solar_system.cpp
@@ -0,0 +1,11 @@
+#include "solar_system.hpp"
+
+void solar_system_t::load( std::fstream &lbfile )
+{
+
+}
+
+void solar_system_t::save( std::fstream &lbfile ) const
+{
+
+}
--- game/world/universe.cpp
+++ game/world/universe.cpp
@@ -1,5 +1,9 @@
 #include "universe.hpp"
 #include "../tasks/help_wanted.h"
+#include <string>
+
+using std::string;
+using std::fstream;
 
 std::unique_ptr<universe_t> universe = std::make_unique<universe_t>();
 
@@ -21,4 +25,37 @@ void save_universe_state ( fstream& lbfile )
     lbfile.write ( reinterpret_cast<const char *> ( &universe->globals.render_graphics ), sizeof ( universe->globals.render_graphics ) );
     
     ai::save_help_wanted( lbfile );
+}
+
+string get_universe_filename() {
+    return string("world/universe.dat");
+}
+
+void universe_t::load()
+{
+    const string filename = get_universe_filename();
+    fstream lbfile(filename, std::ios::in | std::ios::binary);
+    std::size_t n_systems = 0;
+    lbfile.read ( reinterpret_cast<char *> ( n_systems ), sizeof ( n_systems ) );
+    for (std::size_t i=0; i < n_systems; ++i) {
+	int system_idx = 0;
+	lbfile.read ( reinterpret_cast<char *> ( system_idx ), sizeof ( system_idx ) );
+	
+	solar_system_t system;
+	system.load ( lbfile );
+	solar_systems [ system_idx ] = system;
+    }
+}
+
+void universe_t::save() const
+{
+    const string filename = get_universe_filename();
+    fstream lbfile(filename, std::ios::out | std::ios::binary);
+    std::size_t n_systems = solar_systems.size();
+    lbfile.write ( reinterpret_cast<const char *> ( n_systems ), sizeof( n_systems ) );
+    for ( auto it = solar_systems.begin(); it != solar_systems.end(); ++it ) {
+	lbfile.write ( reinterpret_cast<const char *> ( it->second.universe_idx ), sizeof( it->second.universe_idx ) );
+	
+	it->second.save( lbfile );
+    }
 }
