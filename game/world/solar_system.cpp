6c5afd862dcd17be0bef542bb9629d7370f26319
thebracket <herberticus@gmail.com>
2015-12-05 16:07:53 -0600

    Universe-gen has the basic concepts present.

--- game/world/solar_system.cpp
+++ game/world/solar_system.cpp
@@ -1,11 +1,14 @@
 #include "solar_system.hpp"
 
+using std::make_pair;
+
 void solar_system_t::load( std::fstream &lbfile )
 {
     lbfile.read ( reinterpret_cast<char *> ( &universe_idx ), sizeof ( universe_idx ) );
     lbfile.read ( reinterpret_cast<char *> ( &universe_x ), sizeof ( universe_x ) );
     lbfile.read ( reinterpret_cast<char *> ( &universe_y ), sizeof ( universe_y ) );
     lbfile.read ( reinterpret_cast<char *> ( &system_class ), sizeof ( system_class ) );
+    lbfile.read ( reinterpret_cast<char *> ( &binary_system ), sizeof ( binary_system ) );
     
     std::size_t n_bodies = 0;
     lbfile.read ( reinterpret_cast<char *> ( n_bodies ), sizeof ( n_bodies ) );
@@ -23,6 +26,7 @@ void solar_system_t::save( std::fstream &lbfile ) const
     lbfile.write ( reinterpret_cast<const char *> ( &universe_x ), sizeof( universe_x ) );
     lbfile.write ( reinterpret_cast<const char *> ( &universe_y ), sizeof( universe_y ) );
     lbfile.write ( reinterpret_cast<const char *> ( &system_class ), sizeof( system_class ) );
+    lbfile.write ( reinterpret_cast<const char *> ( &binary_system ), sizeof( binary_system ) );
     
     std::size_t n_bodies = bodies.size();
     lbfile.write ( reinterpret_cast<const char *> ( &n_bodies ), sizeof( n_bodies ) );
@@ -34,5 +38,14 @@ void solar_system_t::save( std::fstream &lbfile ) const
 
 std::pair<SDL_Color, unsigned char> solar_system_renderable( const solar_system_t &system)
 {
-    return std::make_pair( SDL_Color{255,255,255,0}, '*' );
+    switch ( system.system_class ) {
+      case O : return make_pair ( SDL_Color { 64, 64, 255, 0}, '`');
+      case B : return make_pair ( SDL_Color { 128, 128, 255, 0}, 249);
+      case A : return make_pair ( SDL_Color { 192, 192, 255, 0}, 15);
+      case F : return make_pair ( SDL_Color { 255, 255, 255, 0}, 15);
+      case G : return make_pair ( SDL_Color { 255, 255, 0, 0}, 15);
+      case K : return make_pair ( SDL_Color { 128, 128, 0, 0}, 15);
+      case M : return make_pair ( SDL_Color { 128, 128, 0, 0}, 249);
+      default: return std::make_pair( SDL_Color{255,255,255,0}, 15 );
+    }
 }
--- game/world/universegen.hpp
+++ game/world/universegen.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include "universe.hpp"
+
 class universe_generator {
 public:
-    universe_generator();
-    void make_universe();
+     universe_generator();
+     void make_universe();
+
+private:
+     void detail_star_system ( solar_system_t & system );
+     void add_planetary_bodies ( solar_system_t & system );
+     void add_planetary_body ( int i, solar_system_t &system );
 };
