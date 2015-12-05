f907aa9e48bd71c32c694d2316c10c9ad8b09454
thebracket <herberticus@gmail.com>
2015-12-05 14:52:34 -0600

    Solar systems define class and renderable.

--- game/world/solar_system.cpp
+++ game/world/solar_system.cpp
@@ -9,3 +9,8 @@ void solar_system_t::save( std::fstream &lbfile ) const
 {
 
 }
+
+std::pair<SDL_Color, unsigned char> solar_system_renderable( const solar_system_t &system)
+{
+    return std::make_pair( SDL_Color{255,255,255,0}, '*' );
+}
