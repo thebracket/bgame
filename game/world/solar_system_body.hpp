9c2879dee7f028d3c3f78e64dd5943dd3d7b66ae
thebracket <herberticus@gmail.com>
2015-12-05 15:09:13 -0600

    Serialization of solar system bodies.

--- game/world/solar_system_body.hpp
+++ game/world/solar_system_body.hpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 #include <memory>
+#include <fstream>
+
+enum system_body_t { HABITABLE_PLANET };
 
 /*
  * Represents a planet or other body orbiting a star system.
@@ -9,6 +12,9 @@
 struct solar_system_body_t {
     /* Index within the solar system. */
     uint8_t system_idx;
+    
+    /* Type of system element. */
+    system_body_t body_type;
         
     // Serialization
     void save( std::fstream &lbfile ) const;
