7d26a4c9d81f2c7d3b207938db6baf8dff30a3ce
Herbert Wolverson <herberticus@gmail.com>
2015-12-07 13:08:57 -0600

    Flight path display

--- game/genesis_mode.h
+++ game/genesis_mode.h
@@ -3,7 +3,7 @@
 #include "../engine/engine.h"
 #include "world/universegen.hpp"
 
-enum genesis_stage_t { INIT, UGEN, UDISPLAY, UDISPLAY2 };
+enum genesis_stage_t { INIT, UGEN, UDISPLAY, UDISPLAY2, TRAVEL };
 
 class genesis_mode : public engine::base_mode {
 public:
@@ -17,11 +17,16 @@ private:
     bool spread_humanity = false;
     int starting_x = 0;
     int starting_y = 0;
+    int current_x;
+    int current_y;
+    std::vector<std::pair<int,int>> flight_path;
+    int flight_progress = 0;
     
     void begin_genesis( const double time_elapsed );
     void ugen( const double time_elapsed );
     void udisplay( const double time_elapsed );
     void udisplay2( const double time_elapsed );
+    void travel( const double time_elapsed );
     
     void show_universe_map( const bool show_warzone, const bool show_truce=false );
     void intro_quote();
