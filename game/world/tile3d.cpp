178f8f99e1936a8aee77453fdb337ada8a94b236
Herbert Wolverson <herberticus@gmail.com>
2015-12-08 15:50:56 -0600

    Work-in-progress.

---
+++ game/world/tile3d.cpp
@@ -0,0 +1,2 @@
+#include "tile3d.hpp"
+
---
+++ game/world/world_defs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+constexpr uint8_t WORLD_WIDTH = 16;
+constexpr uint8_t WORLD_HEIGHT = 16;
+constexpr uint16_t WORLD_TILES = WORLD_HEIGHT * WORLD_WIDTH;
+
+constexpr uint8_t REGION_WIDTH = 254;
+constexpr uint8_t REGION_HEIGHT = 254;
+constexpr uint16_t REGION_TILES = REGION_HEIGHT * REGION_WIDTH;
