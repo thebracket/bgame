163fd5e89b56ba33aedb8db1c660cec88e40168b
thebracket <herberticus@gmail.com>
2015-12-08 16:29:48 -0600

    Work-in-progress.

--- game/world/planet.cpp
+++ game/world/planet.cpp
@@ -1,2 +1,25 @@
 #include "planet.hpp"
 
+region_t* planet_t::get_region ( const uint8_t region_index )
+{
+    if ( regions[ region_index ].get() == nullptr ) {
+      load_region( region_index );
+    }
+    return regions[ region_index ].get();
+}
+
+tile_t* planet_t::get_tile ( const location_t location )
+{
+    region_t * region = get_region( location.region );
+    return region->get_tile( location.x, location.y, location.z );
+}
+
+void planet_t::load_region ( const uint8_t region_index )
+{
+
+}
+
+void planet_t::save_region ( const uint8_t region_index )
+{
+
+}
--- game/world/planet.hpp
+++ game/world/planet.hpp
@@ -9,7 +9,7 @@
 class planet_t {
 public:
   planet_t() { regions.resize( WORLD_TILES ); }
-  region_t * get_region ( const uint8_t * region_index );
+  region_t * get_region ( const uint8_t region_index );
   inline uint8_t planet_idx ( const uint8_t &x, const uint8_t &y) { return (y * WORLD_WIDTH) + x; }
   tile_t * get_tile( const location_t location );
 private:
