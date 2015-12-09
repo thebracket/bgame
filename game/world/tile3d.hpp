bc2c3c44b1372339f88b7c71d18bf83367898c5e
Herbert Wolverson <herbert@iMac.local>
2015-12-09 11:38:44 -0600

    WIP

--- game/world/tile3d.hpp
+++ game/world/tile3d.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "tile_types.hpp"
+
 class tile_t {
 public:
-  
+    bool solid;
+    unsigned char base_tile_type;
+    unsigned char ground;
+    unsigned char climate;
+    unsigned char covering;
 };
