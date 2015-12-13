71b49870c446697cdcb4d0a975b99df66ec8d9d0
thebracket <herberticus@gmail.com>
2015-12-13 17:12:33 -0600

    Wire up camera system using the new position3d system

--- game/systems/camera_system.cpp
+++ game/systems/camera_system.cpp
@@ -2,31 +2,31 @@
 #include "../game.h"
 
 void camera_system::tick(const double &duration_ms) {
-    position_component * camera_pos = game_engine->ecs->find_entity_component<position_component>(world::camera_handle);
+    position_component3d * camera_pos = game_engine->ecs->find_entity_component<position_component3d>(world::camera_handle);
 
     for (command_message &cmd : *game_engine->messaging->get_messages_by_type<command_message>() ) {
 	  if (!cmd.deleted and cmd.command == CAMERA_UP) {
 	      cmd.deleted = true;
-	      camera_pos->y--;
-	      if (camera_pos->y < 0) camera_pos->y = 0;
+	      camera_pos->pos.y--;
+	      if (camera_pos->pos.y < 0) camera_pos->pos.y = 0;
 	      game_engine->messaging->add_message<entity_moved_message>( entity_moved_message() );
 	  }
 	  if (!cmd.deleted and cmd.command == CAMERA_DOWN) {
 	      cmd.deleted = true;
-	      camera_pos->y++;
-	      if (camera_pos->y > landblock_height) camera_pos->y = landblock_height;
+	      camera_pos->pos.y++;
+	      if (camera_pos->pos.y > landblock_height) camera_pos->pos.y = landblock_height;
 	      game_engine->messaging->add_message<entity_moved_message>( entity_moved_message() );
 	  }
 	  if (!cmd.deleted and cmd.command == CAMERA_LEFT) {
 	      cmd.deleted = true;
-	      camera_pos->x--;
-	      if (camera_pos->x < 0) camera_pos->x = 0;
+	      camera_pos->pos.x--;
+	      if (camera_pos->pos.x < 0) camera_pos->pos.x = 0;
 	      game_engine->messaging->add_message<entity_moved_message>( entity_moved_message() );
 	  }
 	  if (!cmd.deleted and cmd.command == CAMERA_RIGHT) {
 	      cmd.deleted = true;
-	      camera_pos->x++;
-	      if (camera_pos->x > landblock_width) camera_pos->x = landblock_width;
+	      camera_pos->pos.x++;
+	      if (camera_pos->pos.x > landblock_width) camera_pos->pos.x = landblock_width;
 	      game_engine->messaging->add_message<entity_moved_message>( entity_moved_message() );
 	  }
      }    
