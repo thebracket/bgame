6c2f1d94f03e13f39743fc2dbb7280a0139af316
Herbert Wolverson <herbert@iMac.local>
2015-12-14 11:37:42 -0600

    Generates a decent noise-based terrain.

--- game/world/gen/world_gen.cpp
+++ game/world/gen/world_gen.cpp
@@ -4,6 +4,7 @@
 #include "world_gen_hydrology.hpp"
 #include "../../../engine/rng.h"
 #include "world_gen_layer_cake.hpp"
+#include "world_gen_noise.hpp"
 #include <iostream>
 #include "../../game.h"
 #include "../universe.hpp"
@@ -18,17 +19,21 @@ void world_gen_phase_1()
     std::unique_ptr<heightmap_t> base_map = get_height_map();
     std::cout << "Zero Height Map\n";
     zero_height_map( base_map.get() );
-    std::cout << "Fault Lines\n";
-    add_fault_lines( base_map.get(), &rng );
+    
+    std::cout << "Making a noise-based height-map\n";
+    make_noisy_heightmap( base_map.get(), &rng );
+    smooth_height_map( base_map.get() );
+        
+    //std::cout << "Fault Lines\n";
+    //add_fault_lines( base_map.get(), &rng );
     //std::cout << "Hydrology\n";
     //std::unique_ptr<water_level_map_t> water = perform_hydrology( base_map.get(), &rng );
-    std::cout << "Smooth\n";
-    for (int i=0; i<128; ++i)
-	smooth_height_map( base_map.get() );    
+    //std::cout << "Smooth\n";
+    //for (int i=0; i<128; ++i) smooth_height_map( base_map.get() );    
     std::cout << "Layercake\n";
     std::unique_ptr<planet_t> planet = make_world_layers( base_map.get(), rng );
-    std::cout << "Making starting entites\n";
     
+    std::cout << "Making starting entites\n";    
     // Make the camera
     entity camera = game_engine->ecs->add_entity();
     game_engine->ecs->add_component(camera, position_component3d({ planet->planet_idx( WORLD_WIDTH/2, WORLD_HEIGHT-1), REGION_WIDTH/2, REGION_HEIGHT/2, 32 }));
