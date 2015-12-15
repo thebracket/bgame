a18c57dd47ecf9de7133fbfcaa712039fd3cec81
thebracket <herberticus@gmail.com>
2015-12-14 20:11:32 -0600

    Biome picking.

--- game/world/gen/world_gen_biomes.cpp
+++ game/world/gen/world_gen_biomes.cpp
@@ -71,10 +71,73 @@ biome_map_t make_biome_map ( heightmap_t* heightmap, engine::random_number_gener
 	const int dx = std::abs ( points[i].first - start_x );
 	const int dy = std::abs ( points[i].second - start_y );
 	const double distance_from_start = std::sqrt( (dx*dx) + (dy*dy) );
-	// Determine climate
+	constexpr double height = WORLD_HEIGHT * REGION_HEIGHT;
+	const double temperature_c_base = -40.0 + ( ( static_cast<double>(points[i].second) / height )*80.0 ) - (BIOME_AVERAGE_ALTITUDE/100.0);
 	// Determine distance from starting point (difficulty)
 	
-	std::cout << i << " ... Altitude Average: " << BIOME_AVERAGE_ALTITUDE << "/" << average_altitude << ". Wetness: " << AVERAGE_WETNESS << ". Distance: " << distance_from_start << "\n";
+	if ( temperature_c_base < -20 ) {
+	    result.biomes [i].climate = tile_climate::ARCTIC;
+	} else if ( temperature_c_base < -10) {
+	    result.biomes [i].climate = tile_climate::SUBARCTIC;
+	} else if ( temperature_c_base < 0) {
+	    result.biomes [i].climate = tile_climate::COLD;
+	} else if ( temperature_c_base < 10) {
+	    result.biomes [i].climate = tile_climate::COOL;
+	} else if ( temperature_c_base < 20) {
+	    result.biomes [i].climate = tile_climate::TEMPERATE;
+	} else if ( temperature_c_base < 30) {
+	    result.biomes [i].climate = tile_climate::WARM;
+	} else if ( temperature_c_base < 40) {
+	    result.biomes [i].climate = tile_climate::HOT;
+	} else {
+	    result.biomes [i].climate = tile_climate::VERY_HOT;
+	}
+	result.biomes[i].difficulty = distance_from_start / 300;
+	    
+	if (result.biomes[i].climate == tile_climate::ARCTIC) {
+	  result.biomes[i].biome_type = biome_types::TUNDRA;
+	}
+	if (result.biomes[i].climate == tile_climate::SUBARCTIC) {
+	    if (AVERAGE_WETNESS > 0.3) {
+		result.biomes[i].biome_type = biome_types::SWAMP;
+	    } else {
+		result.biomes[i].biome_type = biome_types::TUNDRA;
+	    }
+	}
+	if (result.biomes[i].climate == tile_climate::COLD or result.biomes[i].climate == tile_climate::COOL or result.biomes[i].climate == tile_climate::TEMPERATE
+	  or result.biomes[i].climate == tile_climate::WARM
+	) {
+	    if (AVERAGE_WETNESS > 0.3) {
+		result.biomes[i].biome_type = biome_types::SWAMP;
+	    } else {
+		if (BIOME_AVERAGE_ALTITUDE < average_altitude) {
+		    int dice = rng->roll_dice(1,3);
+		    switch (dice) {
+		      case 1 : result.biomes[i].biome_type = biome_types::FLATLAND; break;
+		      case 2 : result.biomes[i].biome_type = biome_types::WOODS; break;
+		      case 3 : result.biomes[i].biome_type = biome_types::FOREST; break;		      
+		    }
+		} else {
+		  int dice = rng->roll_dice(1,2);
+		  switch (dice) {
+		      case 1 : result.biomes[i].biome_type = biome_types::HILLS; break;
+		      case 2 : result.biomes[i].biome_type = biome_types::WOODS; break;
+		    }
+		}
+	    }
+	}
+	if (result.biomes[i].climate == tile_climate::HOT or result.biomes[i].climate == tile_climate::VERY_HOT) {
+	  if (AVERAGE_WETNESS < 0.01) {
+	    result.biomes[i].biome_type = biome_types::DESERT;
+	  } else if (AVERAGE_WETNESS < 0.05) {
+	    result.biomes[i].biome_type = biome_types::BADLANDS;	    
+	  } else {
+	    result.biomes[i].biome_type = biome_types::JUNGLE;
+	  }
+	}
+	
+	std::cout << i << " ... Altitude Average: " << BIOME_AVERAGE_ALTITUDE << "/" << average_altitude << ". Wetness: " << AVERAGE_WETNESS << ". Distance: " << distance_from_start << ". Temp: " << temperature_c_base << "C\n";
+	result.biomes[i].dump();
     }
     
     result.biome_map = std::move( vmap_p );
--- game/world/gen/world_gen_biomes.hpp
+++ game/world/gen/world_gen_biomes.hpp
@@ -3,15 +3,48 @@
 #include "world_height_map.hpp"
 #include "../../../engine/rng.h"
 #include "world_gen_hydrology.hpp"
+#include "../tile_types.hpp"
 #include <memory>
 #include <vector>
 #include <utility>
+#include <iostream>
 
 struct biome_t {
     std::vector<std::pair<uint16_t,uint16_t>> points;
     uint8_t climate;
     uint8_t difficulty;
     uint8_t biome_type;
+    
+    void dump() {
+	std::cout << "Climate: ";
+	switch (climate) {
+	  case tile_climate::ARCTIC : std::cout << "Arctic"; break;
+	  case tile_climate::SUBARCTIC : std::cout << "Sub-arctic"; break;
+	  case tile_climate::COLD : std::cout << "Cold"; break;
+	  case tile_climate::COOL : std::cout << "Cool"; break;
+	  case tile_climate::TEMPERATE : std::cout << "Temperate"; break;
+	  case tile_climate::WARM : std::cout << "Warm"; break;
+	  case tile_climate::HOT : std::cout << "Hot"; break;
+	  case tile_climate::VERY_HOT : std::cout << "Very Hot"; break;
+	  default : std::cout << "Other";
+	}
+	
+	std::cout << ", ";
+	switch (biome_type) {
+	  case biome_types::TUNDRA : std::cout << "Tundra"; break;
+	  case biome_types::DESERT : std::cout << "Desert"; break;
+	  case biome_types::JUNGLE : std::cout << "Jungle"; break;
+	  case biome_types::BADLANDS : std::cout << "Badlands"; break;
+	  case biome_types::SWAMP : std::cout << "Swamp"; break;
+	  case biome_types::FLATLAND : std::cout << "Flatland"; break;
+	  case biome_types::WOODS : std::cout << "Woods"; break;
+	  case biome_types::FOREST : std::cout << "Forest"; break;
+	  case biome_types::HILLS : std::cout << "Hills"; break;
+	}
+	
+	std::cout << ", Difficulty: " << difficulty;
+	std::cout << "\n";
+    }
 };
 
 struct biome_map_t {
