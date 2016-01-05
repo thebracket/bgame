5eecbab98302b3f3e87d518453364d796b01c4ea
Herbert Wolverson <herberticus@gmail.com>
2016-01-05 11:43:31 -0600

    Add comments to the height-map code.

--- src/game/world/gen/world_height_map.hpp
+++ src/game/world/gen/world_height_map.hpp
@@ -10,6 +10,9 @@
 
 using heightmap_t = std::vector< int >;
 
+/*
+ * Creates a new height map
+ */
 inline std::unique_ptr<heightmap_t> get_height_map()
 {
 	std::unique_ptr<heightmap_t> result = std::make_unique<heightmap_t>();
@@ -17,6 +20,9 @@ inline std::unique_ptr<heightmap_t> get_height_map()
 	return std::move(result);
 }
 
+/*
+ * Deep-copies a height-map, and returns the new copy.
+ */
 inline std::unique_ptr<heightmap_t> copy_height_map(heightmap_t * original)
 {
 	std::unique_ptr<heightmap_t> result = std::make_unique<heightmap_t>();
@@ -24,27 +30,59 @@ inline std::unique_ptr<heightmap_t> copy_height_map(heightmap_t * original)
 	return std::move(result);
 }
 
+/*
+ * Quick calculation for height-map indices.
+ */
 inline int height_map_idx(const int x, const int y)
 {
 	return (y * WORLD_WIDTH * REGION_WIDTH) + x;
 }
 
+/*
+ * Fill an entire height-map with a specified value.
+ */
 inline void fill_height_map(heightmap_t * height_map, const uint16_t value)
 {
 	std::fill(height_map->begin(), height_map->end(), value);
 }
 
+/*
+ * Fill a height-map with zeroes.
+ */
 inline void zero_height_map(heightmap_t * height_map)
 {
 	fill_height_map(height_map, 0);
 }
 
+/*
+ * Calculate the average height of a height-map.
+ */
 inline int average_heightmap_height(heightmap_t * height_map)
 {
 	int sum = std::accumulate(height_map->begin(), height_map->end(), 0);
 	return sum / height_map->size();
 }
 
+/*
+ * Calculates the lowest height-map height.
+ */
+inline int min_heightmap_height(heightmap_t * height_map)
+{
+	return *std::min_element(height_map->begin(), height_map->end());
+}
+
+/*
+ * Calculates the highest height-map height.
+ */
+inline int max_heightmap_height(heightmap_t * height_map)
+{
+	return *std::max_element(height_map->begin(), height_map->end());
+}
+
+/*
+ * Smooth a height-map by averaging each cell with its neighbors. Utilizes
+ * a copy, to avoid artifacts.
+ */
 inline void smooth_height_map(heightmap_t * height_map)
 {
 	std::unique_ptr<heightmap_t> tmp_p = get_height_map();
@@ -71,16 +109,10 @@ inline void smooth_height_map(heightmap_t * height_map)
 	std::copy(tmp_p->begin(), tmp_p->end(), std::back_inserter(*height_map));
 }
 
-inline int min_heightmap_height(heightmap_t * height_map)
-{
-	return *std::min_element(height_map->begin(), height_map->end());
-}
-
-inline int max_heightmap_height(heightmap_t * height_map)
-{
-	return *std::max_element(height_map->begin(), height_map->end());
-}
-
+/*
+ * Determines the altitude at which flooding occurs, aiming for desired_percentage percent
+ * of all tiles in the world to be under water.
+ */
 inline int find_flood_level(heightmap_t * height_map, float desired_percentage)
 {
 	const int n_tiles_required = (desired_percentage
