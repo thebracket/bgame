ea6065ef976d3ee4e26e4e1ceb9833909476c7b8
Herbert Wolverson <herbert@iMac.local>
2016-01-06 10:53:11 -0600

    The sun now has a Y axis as well, it should now be possible to build a working sun dial.

--- src/game/systems/global_illumination_system.cpp
+++ src/game/systems/global_illumination_system.cpp
@@ -16,6 +16,7 @@ void sunlight() {
 	const int right_x = camera_pos->pos.x + ((1024 / 8) / 2);
 
 	const float delta_z = 0.1F;
+	const float delta_y = 0.1F;
 	const float delta_x = (calendar->sun_x / 768.0F)*2.0;
 	//std::cout << "Delta-X: " << delta_x << "\n";
 
@@ -30,6 +31,7 @@ void sunlight() {
 				bool blocked = false;
 				bool clipped = false;
 				float tile_x = x;
+				float tile_y = y;
 				float tile_z = z;
 				int n = 0;
 				int last_idx = -1;
@@ -37,17 +39,19 @@ void sunlight() {
 				//std::cout << "Ray [" << delta_x << "," << delta_z << "]: ";
 				while (!blocked and !clipped and n<20) {
 					tile_x += delta_x;
+					tile_y += delta_y;
 					tile_z += delta_z;
 
 					const int tx = std::round(tile_x);
+					const int ty = std::round(tile_y);
 					const int tz = std::round(tile_z);
 					//std::cout << "{" << tx << "," << tz << "},";
 
-					if (tx < 1 or tx >= REGION_WIDTH or tz < 1 or tz >= REGION_DEPTH or y < 1 or y > REGION_HEIGHT) {
+					if (tx < 1 or tx >= REGION_WIDTH or tz < 1 or tz >= REGION_DEPTH or ty < 1 or ty > REGION_HEIGHT) {
 						clipped = true;
 						//std::cout << "C";
 					} else {
-						const int idx = get_tile_index(tx,y,tz);
+						const int idx = get_tile_index(tx,ty,tz);
 						if (idx != last_idx) {
 							if (region->tiles[idx].flags.test(TILE_OPTIONS::VIEW_BLOCKED) or region->tiles[idx].flags.test(TILE_OPTIONS::SOLID) ) {
 								blocked = true;
