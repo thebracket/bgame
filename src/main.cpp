fe3a2160baba27a56b0ba34631358ce4b8ca3d67
Herbert Wolverson <herberticus@gmail.com>
2016-04-16 10:27:32 -0500

    Reduces the game to hello world, but the new structure is compiling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-#include "game/game.h"
+#include <rltk.hpp>
+#include <sstream>
+
+using namespace rltk;
+using namespace rltk::colors;
+
+void tick(double duration_ms) {
+	console->clear();
+    std::stringstream ss;
+    ss << "Frame duration: " << duration_ms << " ms (" << (1000.0/duration_ms) << " FPS).";
+    console->print(1, 1, "Hello World", WHITE, BLACK);
+    console->print(1, 2, ss.str(), YELLOW, BLUE);
+}
 
 int main()
 {
-	run_game();
+	init(config_simple_px("../assets"));
+    run(tick);
 }
