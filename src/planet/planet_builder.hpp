43b8b621156ad688667d25d84f2e8ad70393029b
Herbert Wolverson <herberticus@gmail.com>
2016-04-19 15:55:58 -0500

    Basic framework for threaded planet generation.

--- src/planet/planet_builder.hpp
+++ src/planet/planet_builder.hpp
@@ -2,3 +2,5 @@
 
 #include "planet.hpp"
 
+void build_planet();
+bool is_planet_build_complete();
--- src/world_gen.hpp
+++ src/world_gen.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <rltk.hpp>
+#include <thread>
+#include <memory>
 
 class world_gen {
 public:
 	void init();
 	void destroy();
 	void tick(const double duration_ms);
+
+	bool done = false;
+private:
+	std::unique_ptr<std::thread> world_thread;
 };
