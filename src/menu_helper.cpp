2d144e8831fea08ad76e59cd261a9bd37effc570
Herbert Wolverson <herberticus@gmail.com>
2016-04-19 08:03:03 -0500

    Display logo on splash/menu and scale it automatically.

--- src/menu_helper.cpp
+++ src/menu_helper.cpp
@@ -11,5 +11,14 @@ void draw_splash_backdrop(layer_t * l, sf::RenderTexture &window) {
         const float y_scale = l->h / 768.0f; 
         backdrop.setScale(sf::Vector2f( x_scale, y_scale ));
         window.draw(backdrop);
+
+        sf::Texture * logo = get_texture("logo");
+        logo->setSmooth(true);
+        logo->setRepeated(false);
+        sf::Sprite logo_sprite(*logo);
+        logo_sprite.setScale(sf::Vector2f( x_scale, y_scale ));
+        float x_pos = (l->w / 2.0f) - ((825.0f * x_scale)/2.0f);
+        logo_sprite.move(sf::Vector2f(x_pos, 0.0f));
+        window.draw(logo_sprite);
 }
 
