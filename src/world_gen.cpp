f60e1108fefc8235c9e73363406d9e100425060a
Herbert Wolverson <herberticus@gmail.com>
2016-04-19 11:36:49 -0500

    Add second layer to world gen screen.

--- src/world_gen.cpp
+++ src/world_gen.cpp
@@ -1,16 +1,19 @@
 #include "world_gen.hpp"
 
+constexpr int WORLD_LAYER=1;
 constexpr int LOG_LAYER=2;
 
 using namespace rltk;
 using namespace rltk::colors;
 
 void world_gen::init() {
+	gui->add_layer(WORLD_LAYER, 0, 0, 800, 600, "8x8", resize_fullscreen, false);
 	gui->add_layer(LOG_LAYER, 0, 0, 800, 600, "8x16", resize_fullscreen, false);
 }
 
 void world_gen::destroy() {
 	gui->delete_layer(LOG_LAYER);
+	gui->delete_layer(WORLD_LAYER);
 }
 
 void world_gen::tick(const double duration_ms) {
