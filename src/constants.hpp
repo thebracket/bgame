d5df2072a87323b5d18b9007a28d19bdac88fc44
Herbert Wolverson <herbert@iMac.local>
2016-04-19 08:03:03 -0500

    Display logo on splash/menu and scale it automatically.

---
+++ src/constants.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+const std::string VERSION = "Version 1.0.0a";
--- src/splash_screen.cpp
+++ src/splash_screen.cpp
@@ -1,6 +1,7 @@
 #include <rltk.hpp>
 #include "splash_screen.hpp"
 #include "menu_helper.hpp"
+#include "constants.hpp"
 #include <iostream>
 
 constexpr int LOG_LAYER=1;
@@ -11,8 +12,8 @@ using namespace rltk::colors;
 
 void splash_screen::tick(const double duration_ms) {
 	term(LOG_LAYER)->clear();
-	term(LOG_LAYER)->print(2, 2, "Black Future 0.0.1a", WHITE, BLACK);
-	term(LOG_LAYER)->print(2, 4, "Loading assets - please wait.", YELLOW, BLACK);
+	term(LOG_LAYER)->print_center(2, VERSION, WHITE, BLACK);
+	term(LOG_LAYER)->print_center(4, "Loading assets - please wait.", YELLOW, BLACK);
 
 	// TODO: Check the loading thread here
 	done_loading = true;
@@ -20,6 +21,7 @@ void splash_screen::tick(const double duration_ms) {
 
 void splash_screen::init() {
 	register_texture("../assets/background_image.png", "backdrop");
+	register_texture("../assets/gamelogo.png", "logo");
 	gui->add_owner_layer(BACKDROP_LAYER, 0, 0, 800, 600, resize_fullscreen, draw_splash_backdrop);
 	gui->add_layer(LOG_LAYER, 0, 0, 800, 600, "8x16", resize_fullscreen, false);
 	// TODO: Start the loader thread.
