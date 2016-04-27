2ae1aa517997a41275112bc107648af384b9316d
Herbert Wolverson <herberticus@gmail.com>
2016-04-27 12:27:14 -0500

    World position expanded to store local coordinates.

--- src/components/world_position.hpp
+++ src/components/world_position.hpp
@@ -5,12 +5,18 @@
 using namespace rltk;
 
 struct world_position_t {
-	int x=0;
-	int y=0;
+	int world_x=0;
+	int world_y=0;
+	int region_x=0;
+	int region_y=0;
+	int region_z=0;
 	std::size_t serialization_identity = 1;
 
 	void save(std::ostream &lbfile) {
-		serialize(lbfile, x);
-		serialize(lbfile, y);
+		serialize(lbfile, world_x);
+		serialize(lbfile, world_y);
+		serialize(lbfile, region_x);
+		serialize(lbfile, region_y);
+		serialize(lbfile, region_z);
 	}
 };
