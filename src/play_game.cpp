3029404f4e1ac14e53972f3b1645040d0dad47c9
Herbert Wolverson <herberticus@gmail.com>
2016-04-27 12:37:08 -0500

    Placeholder for game-play mode.

---
+++ src/play_game.cpp
@@ -0,0 +1,16 @@
+#include "play_game.hpp"
+
+#include <rltk.hpp>
+#include <iostream>
+
+using namespace rltk;
+using namespace rltk::colors;
+
+void play_game::tick(const double duration_ms) {
+}
+
+void play_game::init() {
+}
+
+void play_game::destroy() {
+}
---
+++ src/play_game.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class play_game {
+public:
+	void init();
+	void destroy();
+	void tick(const double duration_ms);
+
+	bool quitting = false;
+};
