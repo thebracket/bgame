cb1656802b8e7a2a6ac381a16ac5318924d379ab
Herbert Wolverson <herberticus@gmail.com>
2016-05-20 10:21:57 -0500

    Add tile content to lua - initial indices.

--- world_defs/tile_content.lua
+++ world_defs/tile_content.lua
@@ -1,1 +1,64 @@
 print('Setting up tile contents')
+tile_content = {
+	nothing = { index = 0 },
+
+	permafrost_white = { index = 1 },
+	permafrost_blue = { index = 2 },
+	permafrost_cracked = { index = 3 },
+	permafrost_dirty = { index = 4 },
+
+	yellow_sand = { index = 5 },
+	yellow_sand_cracked = { index = 6 },
+	yellow_sand_dirty = { index = 7 },
+	yellow_sand_ripples = { index = 8 },
+
+	red_sand = { index = 9 },
+	red_sand_cracked = { index = 10 },
+	red_sand_dirty = { index = 11 },
+	red_sand_ripples = { index = 12 },
+
+	white_sand = { index = 13 },
+	white_sand_cracked = { index = 14 },
+	white_sand_dirty = { index = 15 },
+	white_sand_ripples = { index = 16 },
+
+	pebbles = { index = 17 },
+	gravel = { index = 18 },
+	mud = { index = 19 },
+
+	lychen = { index = 1024 },
+	moss = { index = 1025 },
+	cactus = { index = 1026 },
+	grass = { index = 1027 },
+	grass_sparse = { index = 1028 },
+	reeds = { index = 1029 },
+	gorse = { index = 1030 },
+	wildflower = { index = 1031 },
+	thistle = { index = 1032 },
+	shrub = { index = 1033 },
+	heather = { index = 1034 },
+
+	cordex = { index = 4096 },
+	scanner_console = { index = 4097 },
+	defense_console = { index = 4098 },
+	education_console = { index = 4099 },
+	communications_console = { index = 4100 },
+	ship_floor = { index = 4101 },
+	ship_wall_ew = { index = 4102 },
+	ship_wall_ns = { index = 4103 },
+	ship_wall_se = { index = 4104 },
+	ship_wall_ne = { index = 4105 },
+	ship_wall_nw = { index = 4106 },
+	ship_wall_sw = { index = 4107 },
+	ship_wall = { index = 4108 },
+	ship_updown = { index = 4109 },
+	ship_down = { index = 4110 },
+	ship_up = { index = 4111 },
+	solar_panel = { index = 4112 },
+	cryo_bed = { index = 4113 },
+	storage_locker = { index = 4114 },
+	battery = { index = 4115 },
+	rtg = { index = 4116 },
+	replicator = { index = 4117 }
+
+}
