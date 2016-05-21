05e03c4a75287c0c8ad1e53c88360689d8c64bb4
Herbert Wolverson <herbert@iMac.local>
2016-05-21 11:41:21 -0500

    Start adding back clothing and professions.

---
+++ world_defs/starting_professions.lua
@@ -0,0 +1,80 @@
+starting_professions = {
+	insurance_adjuster = {
+		name = "Insurance Adjuster",
+		modifiers = { int = 2, cha = -1 },
+		clothing = {
+			male = { torso = 'formal_shirt', legs = 'cargo_pants' },
+			female = { torso = 'formal_blouse', legs = "long_skirt" },
+			both = { shoes = "leather_shoes" }
+		}
+	},
+	feng_shui_consultant = {
+		name = "Feng Shui Consultant",
+		modifiers = { wis = 1, cha = 1, str = -1 },
+		clothing = {
+			both = { torso = "bathrobe", legs = "hawaiian_shorts", shoes = "sandals" }
+		}
+	},
+	comic_book_collector = {
+		name = "Comic Book Collector",
+		modifiers = { str = -1, con = -1, int = 2 },
+		clothing = {
+			male = { head = "batman_hat", torso="spandex_shirt", legs="spandex_pants", shoes="combat_boots" },
+			female = { head = "tiara", torso = "spandex_blouse", legs="miniskirt", shoes="high_boots" }
+		}
+	},
+	fantasy_football_expert = {
+		name = "Fantasy Football Expert",
+		modifiers = { int = 1, dex = -1 },
+		clothing = {
+			both = { torso = "sports_shirt", legs = "sports_shorts", shoes = "sneakers" }
+		}
+	},
+	blogger = {
+		name = "Blogger",
+		modifiers = { int = 2, con = -2 },
+		clothing = {
+			both = { torso = "t_shirt", legs = "sports_shorts", shoes = "sneakers" }
+		}
+	},
+	shill = {
+		name = "Paid Internet Shill",
+		modifiers = { cha = 2, con = -2 },
+		clothing = {
+			both = { torso = "t_shirt", legs = "sports_shorts", shoes = "sneakers" }
+		}
+	},
+	cheerleader = {
+		name = "Cheerleader",
+		modifiers = { dex = 2, com = 2, int = -1, wis = -1 },
+		clothing = {
+			male = { torso="spandex_shirt", legs="spandex_pants", shoes="sneakers" },
+			female = { head = "tiara", torso = "spandex_blouse", legs="miniskirt", shoes="sneakers" }
+		}
+	},
+	sports_commentator = {
+		name = "Sports Commentator",
+		modifiers = { cha = 2, int = -2 },
+		clothing = {
+			both = { torso = "sports_shirt", legs = "sports_shorts", shoes = "sneakers" }
+		}
+	},
+	synergy_consultant = {
+		name = "Synergy Consultant",
+		modifiers = { cha = 2, wis = -2 },
+		clothing = {
+			male = { torso = 'formal_shirt', legs = 'cargo_pants' },
+			female = { torso = 'formal_blouse', legs = "long_skirt" },
+			both = { shoes = "leather_shoes" }
+		}
+	},
+	used_car_dealer = {
+		name = "Used Car Dealer",
+		modifiers = { cha = 2, eth = -2 },
+		clothing = {
+			male = { torso = 'formal_shirt', legs = 'cargo_pants' },
+			female = { torso = 'formal_blouse', legs = "long_skirt" },
+			both = { shoes = "leather_shoes" }
+		}
+	},
+}
