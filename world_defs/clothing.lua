cd19e444a955c1d25ab5a0f0914f0a6aaabb5f6a
Herbert Wolverson <herberticus@gmail.com>
2016-05-21 11:41:21 -0500

    Start adding back clothing and professions.

---
+++ world_defs/clothing.lua
@@ -0,0 +1,92 @@
+var clothing = {
+	batman_hat = {
+		slot = "head",
+		colors = {"black"},
+		description = "A black helmet with bat-ear protrusions and a face-mask. Be the hero Cordex needs!"
+	},
+	formal_shirt = {
+		slot = "torso",
+		colors = {"white"},
+		description = "A nice shirt with a collar and button-up cuffs. Ideal for the office."		
+	},
+	formal_blouse = {
+		slot = "torso",
+		colors = {"white"},
+		description = "A nice blouse with a collar and button-up cuffs. Ideal for the office."		
+	},
+	bathrobe = {
+		slot = "torso",
+		colors = {"red", "blue", "yellow"},
+		description = "A comfortable, silk bath-robe. Ideal for running a sleazy mansion, pretending to be a wizard, or taking a bath."
+	},
+	spandex_shirt = {
+		slot = "torso",
+		colors = {"red", "blue", "yellow", "grey", "green"},
+		description = "A tight, spandex shirt. Ideal for the gymnasium, or unleashing your inner super-hero."
+	},
+	spandex_blouse = {
+		slot = "torso",
+		colors = {"red", "blue", "yellow", "grey", "green"},
+		description = "A tight, spandex blouse. Ideal for the gymnasium, or unleashing your inner super-hero."
+	},
+	sports_shirt = {
+		slot = "torso",
+		colors = {"red", "blue", "yellow", "grey", "green", "yellow"},
+		description = "A sporting shirt, showing the colors of your favorite team."
+	},
+	cargo_pants = {
+		slot = "legs",
+		colors = {"grey", "khaki", "blue", "navy", "black"},
+		description = "A pair of sturdy trousers, featuring many pockets. Practical and comfortable, and who knows what will be found in the pockets?"
+	},
+	long_skirt = {
+		slot = "legs",
+		colors = {"grey", "khaki", "blue", "navy", "black"},
+		description = "A formal skirt, covering from waist to knees."
+	},
+	hawaiian_shorts = {
+		slot = "legs",
+		colors = {"red", "yellow", "blue", "green", "pink"},
+		description = "A pair of silky shorts, colored to ensure visibility even from orbit. Never lose your waist again."
+	},
+	sports_shorts = {
+		slot = "legs",
+		colors = {"red", "yellow", "blue", "green", "pink"},
+		description = "A pair of sporting shorts, ideal for exercise, running away, or following your favorite team."
+	},
+	spandex_pants = {
+		slot = "legs",
+		colors = {"red", "blue", "yellow", "grey", "green"},
+		description = "Tight, spandex pants. Show off your legs, or horrify your neighbors."
+	},
+	miniskirt = {
+		slot = "legs",
+		colors = {"red", "blue", "yellow", "grey", "green", "grey", "black", "white"},
+		description = "A short skirt, leaving little to the imagination."
+	},
+	leather_shoes = {
+		slot = "shoes",
+		colors = {"brown", "black"},
+		description = "A pair of sturdy leather shoes. Very boring, and very practical."
+	},
+	sandals = {
+		slot = "shoes",
+		colors = {"brown", "black"},
+		description = "A pair of sandals, offering minimal protection to the feet and freeing the wearer from the tyranny of shoelaces."
+	},
+	combat_boots = {
+		slot = "shoes",
+		colors = {"brown", "black"},
+		description = "Practical leather boots, with reinforced ankles. Kick some butt. Available in a wide range of TactiCool(TM) designs."
+	},
+	high_boots = {
+		slot = "shoes",
+		colors = {"brown", "black", "red", "green", "yellow"},
+		description = "Leather boots that come up over the knee. Great for fishing, looking good, or kicking some butt."
+	},
+	sneakers = {
+		slot = "shoes",
+		colors = {"brown", "black", "red", "green", "yellow"},
+		description = "Comfortable running shoes, complete with oxygenated layer to protect your feet and a patented No-Stink technology."
+	}
+}
