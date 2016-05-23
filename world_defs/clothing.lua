aeedb4c93f8f73550c5d8153c9b511bd9d8f0e06
Herbert Wolverson <herberticus@gmail.com>
2016-05-23 14:59:45 -0500

    Start read code for raw clothing.

--- world_defs/clothing.lua
+++ world_defs/clothing.lua
@@ -1,100 +1,120 @@
 clothing = {
 	batman_hat = {
+		name = "Batman Hat",
 		slot = "head",
 		colors = {"black"},
 		description = "A black helmet with bat-ear protrusions and a face-mask. Be the hero Cordex needs!"
 	},
 	police_hat = {
+		name = "Police Hat",
 		slot = "head",
 		colors = {"black"},
 		description = "A black, flat hat. Typically part of a uniform."
 	},
 	cat_ears = {
+		name = "Cat Ears",
 		slot = "head",
 		colors = {"black"},
 		description = "Clip on, prosthetic cat ears (not made from a real cat)."
 	},
 	formal_shirt = {
+		name = "Formal Shirt",
 		slot = "torso",
 		colors = {"white"},
 		description = "A nice shirt with a collar and button-up cuffs. Ideal for the office."		
 	},
 	formal_blouse = {
+		name = "Formal Blouse",
 		slot = "torso",
 		colors = {"white"},
 		description = "A nice blouse with a collar and button-up cuffs. Ideal for the office."		
 	},
 	bathrobe = {
+		name = "Bathrobe",
 		slot = "torso",
 		colors = {"red", "blue", "yellow"},
 		description = "A comfortable, silk bath-robe. Ideal for running a sleazy mansion, pretending to be a wizard, or taking a bath."
 	},
 	spandex_shirt = {
+		name = "Spandex Shirt",
 		slot = "torso",
 		colors = {"red", "blue", "yellow", "grey", "green"},
 		description = "A tight, spandex shirt. Ideal for the gymnasium, or unleashing your inner super-hero."
 	},
 	spandex_blouse = {
+		name = "Spandex Blouse",
 		slot = "torso",
 		colors = {"red", "blue", "yellow", "grey", "green"},
 		description = "A tight, spandex blouse. Ideal for the gymnasium, or unleashing your inner super-hero."
 	},
 	sports_shirt = {
+		name = "Sports Shirt",
 		slot = "torso",
 		colors = {"red", "blue", "yellow", "grey", "green", "yellow"},
 		description = "A sporting shirt, showing the colors of your favorite team."
 	},
 	cargo_pants = {
+		name = "Cargo Pants",
 		slot = "legs",
 		colors = {"grey", "khaki", "blue", "navy", "black"},
 		description = "A pair of sturdy trousers, featuring many pockets. Practical and comfortable, and who knows what will be found in the pockets?"
 	},
 	long_skirt = {
+		name = "Long Skirt",
 		slot = "legs",
 		colors = {"grey", "khaki", "blue", "navy", "black"},
 		description = "A formal skirt, covering from waist to knees."
 	},
 	hawaiian_shorts = {
+		name = "Hawaiian Shorts",
 		slot = "legs",
 		colors = {"red", "yellow", "blue", "green", "pink"},
 		description = "A pair of silky shorts, colored to ensure visibility even from orbit. Never lose your waist again."
 	},
 	sports_shorts = {
+		name = "Sports Shorts",
 		slot = "legs",
 		colors = {"red", "yellow", "blue", "green", "pink"},
 		description = "A pair of sporting shorts, ideal for exercise, running away, or following your favorite team."
 	},
 	spandex_pants = {
+		name = "Spandex Pants",
 		slot = "legs",
 		colors = {"red", "blue", "yellow", "grey", "green"},
 		description = "Tight, spandex pants. Show off your legs, or horrify your neighbors."
 	},
 	miniskirt = {
+		name = "Miniskirt",
 		slot = "legs",
 		colors = {"red", "blue", "yellow", "grey", "green", "grey", "black", "white"},
 		description = "A short skirt, leaving little to the imagination."
 	},
 	leather_shoes = {
+		name = "Leather Shoes",
 		slot = "shoes",
 		colors = {"brown", "black"},
 		description = "A pair of sturdy leather shoes. Very boring, and very practical."
 	},
 	sandals = {
+		name = "Sandals",
 		slot = "shoes",
 		colors = {"brown", "black"},
 		description = "A pair of sandals, offering minimal protection to the feet and freeing the wearer from the tyranny of shoelaces."
 	},
 	combat_boots = {
+		name = "Combat Boots",
 		slot = "shoes",
 		colors = {"brown", "black"},
 		description = "Practical leather boots, with reinforced ankles. Kick some butt. Available in a wide range of TactiCool(TM) designs."
 	},
 	high_boots = {
+		name = "High Boots",
 		slot = "shoes",
 		colors = {"brown", "black", "red", "green", "yellow"},
 		description = "Leather boots that come up over the knee. Great for fishing, looking good, or kicking some butt."
 	},
 	sneakers = {
+		name = "Sneakers",
 		slot = "shoes",
 		colors = {"brown", "black", "red", "green", "yellow"},
 		description = "Comfortable running shoes, complete with oxygenated layer to protect your feet and a patented No-Stink technology."
