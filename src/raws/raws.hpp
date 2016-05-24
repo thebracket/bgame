219d7a1f0b61581642bf0e803dd70ef6be514283
Herbert Wolverson <herberticus@gmail.com>
2016-05-24 13:48:29 -0500

    Starting professions are loaded properly.

--- src/raws/raws.hpp
+++ src/raws/raws.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <tuple>
 #include <rltk.hpp>
 
 struct string_table_t {
@@ -47,7 +48,7 @@ struct profession_t {
 	short charisma = 0;
 	short comeliness = 0;
 	short ethics;
-	std::vector< std::pair< uint8_t, std::string >> starting_clothes;
+	std::vector< std::tuple< uint8_t, std::string, std::string >> starting_clothes;
 };
 
 extern string_table_t first_names_male;
@@ -60,6 +61,7 @@ extern std::unordered_map<int, tile_content_t> tile_contents;
 extern std::unordered_map<std::string, int> tile_contents_index;
 
 extern std::unordered_map<std::string, clothing_t> clothing_types;
+extern std::vector<profession_t> starting_professions;
 
 void load_raws();
 int get_tile_type_index(const std::string name);
