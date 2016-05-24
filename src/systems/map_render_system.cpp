4cf0fa0957da7e4d92a22273bf3f1eb51805ef90
Herbert Wolverson <herbert@iMac.local>
2016-05-24 15:12:43 -0500

    Inefficient but working renderables list, displays renderable entities.

--- src/systems/map_render_system.cpp
+++ src/systems/map_render_system.cpp
@@ -1,5 +1,4 @@
 #include "map_render_system.hpp"
-#include "../game_globals.hpp"
 #include "../raws/raws.hpp"
 #include <iostream>
 
@@ -44,6 +43,13 @@ void map_render_system::update(const double duration_ms) {
 	}
 
 	if (clip_left == -1) update_clipping_rectangle();
+
+	for (auto i : renderables) {
+		i.reset();
+	}
+	each<renderable_t, position_t>([this] (entity_t &entity, renderable_t &render, position_t &pos) {
+		this->renderables[current_region.idx(pos.x, pos.y, pos.z)] = rltk::vchar{render.glyph, render.foreground, render.background};
+	});
 
 	if (dirty) {
 		term(1)->clear();
@@ -91,19 +97,11 @@ vchar map_render_system::get_render_char_for_base(const uint8_t base_type) const
 	} else {
 		return vchar{'?', rltk::colors::MAGENTA, rltk::colors::BLACK};
 	}
-	/*
-	switch (base_type) {
-		case tile_type::NOTHING : return vchar{' ', rltk::colors::GREY, rltk::colors::BLACK};
-		case tile_type::SEMI_MOLTEN_ROCK : return vchar{177, rltk::colors::RED, rltk::colors::YELLOW};
-		case tile_type::ROCK : return vchar{219, rltk::colors::GREY, rltk::colors::BLACK};
-		case tile_type::SOIL : return vchar{176, rltk::colors::Brown, rltk::colors::BLACK};
-		case tile_type::YELLOW_SAND : return vchar{176, rltk::colors::YELLOW, rltk::colors::BLACK};
-		case tile_type::RED_SAND : return vchar{176, rltk::colors::RED, rltk::colors::BLACK};
-		default : return vchar{'?', rltk::colors::MAGENTA, rltk::colors::BLACK};
-	}*/
 }
 
 vchar map_render_system::get_render_char(const int idx) const {
+	if (renderables[idx]) return renderables[idx].get();
+
 	if (current_region.tiles[idx].flags.test(tile_flags::SOLID) && !current_region.tiles[idx].flags.test(tile_flags::CONSTRUCTION)) {
 		return get_render_char_for_base(current_region.tiles[idx].base_type);
 	} else {
@@ -117,63 +115,6 @@ vchar map_render_system::get_render_char(const int idx) const {
 				return vchar{'?', rltk::colors::MAGENTA, rltk::colors::BLACK};
 			}
 		}
-		/*
-		switch (current_region.tiles[idx].contents) {
-			case tile_content::NOTHING : return get_render_char_for_base(current_region.tiles[idx].base_type);
-			case tile_content::PERMAFROST_WHITE : return vchar{178, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::PERMAFROST_BLUE : return vchar{178, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::PERMAFROST_CRACKED : return vchar{178, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::PERMAFROST_DIRTY : return vchar{178, rltk::colors::GREY, rltk::colors::BLACK};
-			case tile_content::YELLOW_SAND : return vchar{176, rltk::colors::YELLOW, rltk::colors::BLACK};
-			case tile_content::YELLOW_SAND_CRACKED : return vchar{176, rltk::colors::YELLOW, rltk::colors::BLACK};
-			case tile_content::YELLOW_SAND_DIRTY : return vchar{177, rltk::colors::YELLOW, rltk::colors::BLACK};
-			case tile_content::YELLOW_SAND_RIPPLES : return vchar{247, rltk::colors::YELLOW, rltk::colors::BLACK};
-			case tile_content::RED_SAND : return vchar{176, rltk::colors::RED, rltk::colors::BLACK};
-			case tile_content::RED_SAND_CRACKED : return vchar{178, rltk::colors::RED, rltk::colors::BLACK};
-			case tile_content::RED_SAND_DIRTY : return vchar{177, rltk::colors::DARK_RED, rltk::colors::BLACK};
-			case tile_content::RED_SAND_RIPPLES : return vchar{247, rltk::colors::RED, rltk::colors::BLACK};
-
-			case tile_content::PEBBLES : return vchar{176, rltk::colors::LIGHT_GREY, rltk::colors::BLACK};
-			case tile_content::WHITE_SAND : return vchar{176, rltk::colors::LIGHTEST_GREY, rltk::colors::BLACK};
-			case tile_content::GRAVEL : return vchar{178, rltk::colors::GREY, rltk::colors::BLACK};
-			case tile_content::MUD : return vchar{178, rltk::colors::Brown, rltk::colors::BLACK};
-			case tile_content::LYCHEN : return vchar{'%', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::MOSS : return vchar{'#', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::CACTUS : return vchar{157, rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::GRASS : return vchar{219, rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::GRASS_SPARSE : return vchar{176, rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::REEDS : return vchar{'&', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::GORSE : return vchar{'^', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::WILDFLOWER : return vchar{'^', rltk::colors::YELLOW, rltk::colors::BLACK};
-			case tile_content::THISTLE : return vchar{'!', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::SHRUB : return vchar{'*', rltk::colors::GREEN, rltk::colors::BLACK};
-			case tile_content::HEATHER : return vchar{'#', rltk::colors::GREEN, rltk::colors::BLACK};
-
-			case tile_content::CORDEX : return vchar{15, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::SCANNER_CONSOLE : return vchar{17, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::DEFENSE_CONSOLE : return vchar{16, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::EDUCATION_CONSOLE : return vchar{30, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::COMMUNICATIONS_CONSOLE : return vchar{31, rltk::colors::CYAN, rltk::colors::BLACK};
-			case tile_content::SHIP_FLOOR : return vchar{176, rltk::colors::GREY, rltk::colors::DARK_GREY};
-			case tile_content::SHIP_WALL_EW : return vchar{205, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL_NS : return vchar{186, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL_SE : return vchar{201, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL_NE : return vchar{200, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL_NW : return vchar{187, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL_SW : return vchar{188, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_WALL : return vchar{219, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::SHIP_UPDOWN : return vchar{'X', rltk::colors::GREY, rltk::colors::BLACK};
-			case tile_content::SHIP_UP : return vchar{'<', rltk::colors::GREY, rltk::colors::BLACK};
-			case tile_content::SHIP_DOWN : return vchar{'>', rltk::colors::GREY, rltk::colors::BLACK};
-			case tile_content::SOLAR_PANEL : return vchar{176, rltk::colors::YELLOW, rltk::colors::CYAN};
-			case tile_content::CRYO_BED : return vchar{'0', rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::STORAGE_LOCKER : return vchar{236, rltk::colors::WHITE, rltk::colors::BLACK};
-			case tile_content::BATTERY : return vchar{241, rltk::colors::LIGHT_RED, rltk::colors::BLACK};
-			case tile_content::RTG : return vchar{206, rltk::colors::ORANGE, rltk::colors::BLACK};
-			case tile_content::REPLICATOR : return vchar{228, rltk::colors::MAGENTA, rltk::colors::BLACK};
-
-			default : return vchar{'?', rltk::colors::MAGENTA, rltk::colors::BLACK};
-		}*/
 	}
 	return vchar{' ', rltk::colors::GREY, rltk::colors::BLACK};
 }
--- src/systems/map_render_system.hpp
+++ src/systems/map_render_system.hpp
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <rltk.hpp>
+#include <vector>
+#include "../game_globals.hpp"
+#include "boost/optional.hpp"
 
 class map_render_system : public rltk::base_system {
 public:
+	map_render_system() { renderables.resize(REGION_WIDTH * REGION_HEIGHT * REGION_DEPTH); }
 	virtual void update(const double duration_ms) override;
 private:
 	bool dirty = true;
 	void update_clipping_rectangle();
 	rltk::vchar get_render_char(const int idx) const;
 	rltk::vchar get_render_char_for_base(const uint8_t base_type) const;
+	std::vector<boost::optional<rltk::vchar>> renderables;
 };
