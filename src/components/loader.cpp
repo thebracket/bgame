956ca764e3f9a898810cc9f54b079335eac129a4
Herbert Wolverson <herberticus@gmail.com>
2016-05-24 15:12:43 -0500

    Inefficient but working renderables list, displays renderable entities.

--- src/components/loader.cpp
+++ src/components/loader.cpp
@@ -1,6 +1,7 @@
 #include "loader.hpp"
 #include "components.hpp"
 #include <rltk.hpp>
+#include <iostream>
 
 void component_loader(std::istream &lbfile, std::size_t type_id, std::size_t entity_id) {
 	switch (type_id) {
