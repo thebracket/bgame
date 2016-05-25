d1ee28db251b4112ffe3fd0c8e9aabb831154378
Herbert Wolverson <herberticus@gmail.com>
2016-05-25 10:56:29 -0500

    Move initiative to its own inline function for clarity.

--- src/systems/settler_ai_system.cpp
+++ src/systems/settler_ai_system.cpp
@@ -9,6 +9,10 @@ using namespace rltk;
 void settler_ai_system::update(const double duration_ms) {
 }
 
+inline void settler_calculate_initiative(settler_ai_t &ai, game_stats_t &stats) {
+	ai.initiative = rng.roll_dice(1, 6) - stat_modifier(stats.dexterity);
+}
+
 void settler_ai_system::configure() {
 	subscribe<tick_message>([this](tick_message &msg) {
 		each<settler_ai_t, game_stats_t, species_t, position_t, name_t>([] (entity_t &entity, settler_ai_t &ai, game_stats_t &stats, 
@@ -17,7 +21,7 @@ void settler_ai_system::configure() {
 			if (ai.initiative < 1) {
 				//std::cout << name.first_name << " " << name.last_name << " (" << stats.profession_tag << ") is thinking.\n";
 
-				ai.initiative = rng.roll_dice(1, 6) - stat_modifier(stats.dexterity);
+				settler_calculate_initiative(ai, stats);
 			} else {
 				--ai.initiative;
 			}
