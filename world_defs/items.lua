c5c464da7e77b933ad7ae90fd0721874a0cbeac2
Herbert Wolverson <herberticus@gmail.com>
2016-05-28 15:12:27 -0500

    Add some more starting items to the LUA.

--- world_defs/items.lua
+++ world_defs/items.lua
@@ -2,6 +2,33 @@ items = {
 	personal_survival_shelter_kit = {
 		name = "Personal Survival Shelter Kit",
 		description = "A folded-up personal shelter kit, known in non-corporate parlance as a camping set: a tent, bedroll and sleeping bag.",
-		itemtype = "component"
+		itemtype = "component",
+		glyph = glyphs['kit'],
+		foreground = colors['green'],
+		background = colors['black']
+	},
+	camp_fire_kit = {
+		name = "Camping Fire Kit",
+		description = "A plastic campfire, powered by a tiny nuclear reactor. Ideal for your off-world vacation.",
+		itemtype = "component",
+		glyph = glyphs['kit'],
+		foreground = colors['red'],
+		background = colors['black']
+	},
+	fire_axe = {
+		name = "Fire Axe",
+		description = "A simple axe. It has the word FIRE burned into it.",
+		itemtype = "tool",
+		glyph = glyphs['axe'],
+		foreground = colors['white'],
+		background = colors['black']
+	},
+	pickaxe = {
+		name = "Pickaxe",
+		description = "A simple pickaxe, ideal for digging holes.",
+		itemtype = "tool",
+		glyph = glyphs['pick'],
+		foreground = colors['white'],
+		background = colors['black']
 	}
 }
