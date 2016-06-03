1c0895488fc39e55b74daf26597568ddb43582e5
Herbert Wolverson <herberticus@gmail.com>
2016-06-03 10:51:52 -0500

    Lots of bugfixes to mining.

--- src/messages/inventory_changed_message.hpp
+++ src/messages/inventory_changed_message.hpp
@@ -7,7 +7,7 @@ struct inventory_changed_message : public rltk::base_message_t {
 
 struct item_claimed_message : public rltk::base_message_t {
 	item_claimed_message() {}
-	item_claimed_message(const bool &c, const int &i) : claimed(c), id(i) {}
+	item_claimed_message(const std::size_t i, const bool c) : claimed(c), id(i) {}
 	bool claimed;
 	std::size_t id;
 };
--- src/systems/settler_ai_system.hpp
+++ src/systems/settler_ai_system.hpp
@@ -17,5 +17,7 @@ private:
 	void do_leisure_time(entity_t &entity, settler_ai_t &ai, game_stats_t &stats, species_t &species, position_t &pos, name_t &name);
 	void do_work_time(entity_t &entity, settler_ai_t &ai, game_stats_t &stats, species_t &species, position_t &pos, name_t &name);
 
+	void change_job_status(settler_ai_t &ai, name_t &name, const std::string new_status);
+
 	void do_mining(entity_t &entity, settler_ai_t &ai, game_stats_t &stats, species_t &species, position_t &pos, name_t &name);
 };
