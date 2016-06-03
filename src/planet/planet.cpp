fd0b6cc2ab541238a44d25372bd5ae8f85c09ff8
Herbert Wolverson <herberticus@gmail.com>
2016-06-03 15:40:28 -0500

    Use boost::iostreams compression on planet data.

--- src/planet/planet.cpp
+++ src/planet/planet.cpp
@@ -1,8 +1,8 @@
 #include "planet.hpp"
 #include <fstream>
 #include <rltk.hpp>
-//#include <Poco/InflatingStream.h>
-//#include <Poco/DeflatingStream.h>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
 #include <iostream>
 
 using namespace rltk;
@@ -11,27 +11,29 @@ const std::string planet_filename = "world/planet.dat";
 
 void save_planet(const planet_t &planet) {
 	std::fstream lbfile(planet_filename, std::ios::out | std::ios::binary);
-	//Poco::DeflatingOutputStream lbfile(masterfile, Poco::DeflatingStreamBuf::STREAM_GZIP);
-	serialize(lbfile, planet.name);
-	serialize(lbfile, planet.rng_seed);
-	serialize(lbfile, planet.perlin_seed);
-	serialize(lbfile, planet.water_height);
-	serialize(lbfile, planet.plains_height);
-	serialize(lbfile, planet.hills_height);
+	boost::iostreams::filtering_stream<boost::iostreams::output> deflate;
+	deflate.push(boost::iostreams::zlib_compressor());
+    deflate.push(lbfile);
+	serialize(deflate, planet.name);
+	serialize(deflate, planet.rng_seed);
+	serialize(deflate, planet.perlin_seed);
+	serialize(deflate, planet.water_height);
+	serialize(deflate, planet.plains_height);
+	serialize(deflate, planet.hills_height);
 
-	serialize(lbfile, planet.biomes.size());
+	serialize(deflate, planet.biomes.size());
 	for (const biome_t &biome : planet.biomes) {
-		serialize(lbfile, biome.biome_type);
-		serialize(lbfile, biome.name);
-		serialize(lbfile, biome.mean_temperature);
-		serialize(lbfile, biome.climate);
+		serialize(deflate, biome.biome_type);
+		serialize(deflate, biome.name);
+		serialize(deflate, biome.mean_temperature);
+		serialize(deflate, biome.climate);
 	}
 
-	serialize(lbfile, planet.landblocks.size());
+	serialize(deflate, planet.landblocks.size());
 	for (const block_t &block : planet.landblocks) {
-		serialize(lbfile, block.height);
-		serialize(lbfile, block.type);
-		serialize(lbfile, block.biome_idx);
+		serialize(deflate, block.height);
+		serialize(deflate, block.type);
+		serialize(deflate, block.biome_idx);
 	}
 }
 
@@ -39,32 +41,34 @@ planet_t load_planet() {
 	planet_t planet;
 
 	std::fstream lbfile(planet_filename, std::ios::in | std::ios::binary);
-	//Poco::InflatingInputStream lbfile(masterfile, Poco::InflatingStreamBuf::STREAM_GZIP);
-	deserialize(lbfile, planet.name);
-	deserialize(lbfile, planet.rng_seed);
-	deserialize(lbfile, planet.perlin_seed);
-	deserialize(lbfile, planet.water_height);
-	deserialize(lbfile, planet.plains_height);
-	deserialize(lbfile, planet.hills_height);
+	boost::iostreams::filtering_stream<boost::iostreams::input> inflate;
+	inflate.push(boost::iostreams::zlib_decompressor());
+    inflate.push(lbfile);
+	deserialize(inflate, planet.name);
+	deserialize(inflate, planet.rng_seed);
+	deserialize(inflate, planet.perlin_seed);
+	deserialize(inflate, planet.water_height);
+	deserialize(inflate, planet.plains_height);
+	deserialize(inflate, planet.hills_height);
 
 	std::size_t number_of_biomes;
-	deserialize(lbfile, number_of_biomes);
+	deserialize(inflate, number_of_biomes);
 	for (std::size_t i=0; i<number_of_biomes; ++i) {
 		biome_t biome;
-		deserialize(lbfile, biome.biome_type);
-		deserialize(lbfile, biome.name);
-		deserialize(lbfile, biome.mean_temperature);
-		deserialize(lbfile, biome.climate);
+		deserialize(inflate, biome.biome_type);
+		deserialize(inflate, biome.name);
+		deserialize(inflate, biome.mean_temperature);
+		deserialize(inflate, biome.climate);
 		planet.biomes.push_back(biome);
 	}
 
 	std::size_t number_of_landblocks;
-	deserialize(lbfile, number_of_landblocks);
+	deserialize(inflate, number_of_landblocks);
 	for (std::size_t i=0; i<number_of_landblocks; ++i) {
 		block_t block;
-		deserialize(lbfile, block.height);
-		deserialize(lbfile, block.type);
-		deserialize(lbfile, block.biome_idx);
+		deserialize(inflate, block.height);
+		deserialize(inflate, block.type);
+		deserialize(inflate, block.biome_idx);
 		planet.landblocks.push_back(block);
 	}
 
