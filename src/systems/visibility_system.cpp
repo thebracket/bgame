82c2c4021e88c18412463d3bdc79f8300abd937a
Herbert Wolverson <herberticus@gmail.com>
2016-06-06 11:24:36 -0500

    Small optimization to the viewshed algorithm.

--- src/systems/visibility_system.cpp
+++ src/systems/visibility_system.cpp
@@ -4,8 +4,7 @@
 
 using namespace rltk;
 
-inline void reveal(const int &x, const int &y, const int &z, viewshed_t &view) {
-	const int idx = current_region.idx(x,y,z);
+inline void reveal(const int &idx, viewshed_t &view) {
 	current_region.tiles[idx].flags.set(tile_flags::REVEALED);
 	view.visible_cache.push_back(idx);
 }
@@ -14,7 +13,7 @@ inline void internal_pen_view_to(position_t &pos, viewshed_t &view, int x, int y
 	const float dist_square = view.viewshed_radius * view.viewshed_radius;
 
 	line_func_3d_cancellable(pos.x, pos.y, pos.z, pos.x+x, pos.y+y, pos.z+z, [&view, &pos, &dist_square] (int X, int Y, int Z) {
-		reveal(X, Y, Z, view);
+		reveal(current_region.idx(X, Y, Z), view);
 		const float distance = distance3d_squared(pos.x, pos.y, pos.z, X, Y, Z);
 		if (distance < dist_square) {
 			return true;
@@ -28,8 +27,8 @@ inline void internal_view_to(position_t &pos, viewshed_t &view, int x, int y, in
 	const float dist_square = view.viewshed_radius * view.viewshed_radius;
 
 	line_func_3d_cancellable(pos.x, pos.y, pos.z, pos.x+x, pos.y+y, pos.z+z, [&view, &pos, &dist_square] (int X, int Y, int Z) {
-		reveal(X, Y, Z, view);
 		const int idx = current_region.idx(X, Y, Z);
+		reveal(idx, view);
 		const float distance = distance3d_squared(pos.x, pos.y, pos.z, X, Y, Z);
 		if (distance > dist_square) {
 			return false;
@@ -41,7 +40,7 @@ inline void internal_view_to(position_t &pos, viewshed_t &view, int x, int y, in
 void update_penetrating_viewshed(entity_t &e, position_t &pos, viewshed_t &view) {
 	view.visible_cache.clear();
 
-	reveal(pos.x, pos.y, pos.z, view);
+	reveal(current_region.idx(pos.x, pos.y, pos.z), view);
 	for (int z=(0-view.viewshed_radius); z<view.viewshed_radius; ++z) {
 		for (int i=0-view.viewshed_radius; i<view.viewshed_radius; ++i) {
 			internal_pen_view_to(pos, view, i, 0-view.viewshed_radius, z);
@@ -54,7 +53,7 @@ void update_penetrating_viewshed(entity_t &e, position_t &pos, viewshed_t &view)
 
 void update_normal_viewshed(entity_t &e, position_t &pos, viewshed_t &view) {
 	view.visible_cache.clear();
-	reveal(pos.x, pos.y, pos.z, view);
+	reveal(current_region.idx(pos.x, pos.y, pos.z), view);
 	for (int z=(0-view.viewshed_radius); z<view.viewshed_radius; ++z) {
 		for (int i=0-view.viewshed_radius; i<view.viewshed_radius; ++i) {
 			internal_view_to(pos, view, i, 0-view.viewshed_radius, z);
