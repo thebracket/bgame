f727b76f8ac38d6e56bf5714d4d87ba000813926
Herbert Wolverson <herberticus@gmail.com>
2016-06-14 11:34:20 -0500

    Add template reactions LUA.

---
+++ world_defs/reactions.lua
@@ -0,0 +1,17 @@
+reactions = {
+    cut_wooden_planks = {
+        name = "Cut Wooden Planks",
+        workshop = "sawmill",
+        inputs = { {"wood_logs", 1} },
+        outputs = { {"wood_planks", 4} },
+        automatic = true
+    },
+    cut_stone = {
+        name = "Cut Stone Blocks",
+        workshop = "stonecutter",
+        inputs = { {"stonecutter", 1} },
+        outputs = { {"stone_blocks", 4} },
+        automatic = true
+    }
+}
+
