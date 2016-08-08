786ca0709248d38f08a980ac5497802688e5fa35
Herbert Wolverson <herberticus@gmail.com>
2016-08-08 13:34:48 -0500

    Add visible entity set to viewshed.

--- src/components/viewshed.hpp
+++ src/components/viewshed.hpp
@@ -2,6 +2,7 @@
 
 #include <rltk.hpp>
 #include <vector>
+#include "boost/container/flat_set.hpp"
 
 using namespace rltk;
 
@@ -12,6 +13,7 @@ struct viewshed_t {
 	int viewshed_radius = 0;
 	bool penetrating = false;
 	bool good_guy_visibility = true;
+	boost::container::flat_set<std::size_t> visible_entities;
 
 	// Non-persistent
 	std::vector<int> visible_cache;
@@ -22,6 +24,10 @@ struct viewshed_t {
 		serialize(lbfile, viewshed_radius);
 		serialize(lbfile, penetrating);
 		serialize(lbfile, good_guy_visibility);
+		serialize(lbfile, visible_entities.size());
+		for (auto it=visible_entities.begin(); it!=visible_entities.end(); ++it) {
+			serialize(lbfile, *it);
+		}
 	}
 
 	static viewshed_t load(std::istream &lbfile) {
@@ -29,6 +35,13 @@ struct viewshed_t {
 		deserialize(lbfile, c.viewshed_radius);
 		deserialize(lbfile, c.penetrating);
 		deserialize(lbfile, c.good_guy_visibility);
+		std::size_t n_vis;
+		deserialize(lbfile, n_vis);
+		for (std::size_t i=0; i<n_vis; ++i) {
+			std::size_t id;
+			deserialize(lbfile, id);
+			c.visible_entities.insert(id);
+		}
 		return c;
 	}
 };
