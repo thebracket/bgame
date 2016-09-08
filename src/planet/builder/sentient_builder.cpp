dcbb96614e5e8f171e67152d2490ac1e9632e994
Herbert Wolverson <herberticus@gmail.com>
2016-09-08 09:42:34 -0500

    Missed file from commit - oops.

---
+++ src/planet/builder/sentient_builder.cpp
@@ -0,0 +1,36 @@
+#include "sentient_builder.hpp"
+#include "../../raws/raws.hpp"
+#include "../../components/components.hpp"
+
+void create_sentient(const int x, const int y, const int z, rltk::random_number_generator &rng, planet_t &planet, region_t &region, const std::size_t person_id) 
+{
+    species_t species;
+
+    species.tag = planet.civs.unimportant_people[person_id].species_tag;
+    auto species_finder = species_defs.find(species.tag);
+    if (species_finder == species_defs.end()) {
+        std::cout << "WARNING: Could not find raws for [" << species.tag << "]\n";
+        return;
+    }
+    if (planet.civs.unimportant_people[person_id].male) {
+        species.gender = MALE;
+    } else {
+        species.gender = FEMALE;
+    }
+
+    auto sentient = create_entity()
+        ->assign(position_t{x,y,z})
+        ->assign(name_t{ species_finder->second.name, OCCUPATION_NAMES[planet.civs.unimportant_people[person_id].occupation] })
+        ->assign(renderable_t{ species_finder->second.glyph ,rltk::colors::WHITE, rltk::colors::BLACK });
+
+    /*auto sentient = create_entity()
+		->assign(position_t{ x,y,z })
+		->assign(renderable_t{ species_finder->second.glyph ,rltk::colors::WHITE, rltk::colors::BLACK })
+		->assign(name_t{ first_name, last_name })
+		->assign(std::move(species))
+		->assign(std::move(health))
+		->assign(std::move(stats))
+		->assign(std::move(ai))
+		->assign(viewshed_t{ 8, false });
+    */
+}
---
+++ src/planet/builder/sentient_builder.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <rltk.hpp>
+#include "../region.hpp"
+#include "../planet.hpp"
+
+void create_sentient(const int x, const int y, const int z, rltk::random_number_generator &rng, planet_t &planet, region_t &region, const std::size_t person_id);
