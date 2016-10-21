9e02d224e37fae5b63e977adfb41f7166d53a50f
Herbert Wolverson <herberticus@gmail.com>
2016-10-21 13:14:43 -0500

    Screenshots

--- src/systems/keyboard_system.cpp
+++ src/systems/keyboard_system.cpp
@@ -13,6 +13,18 @@ void keyboard_system::update(const double ms) {
     while (!messages->empty()) {
 		key_pressed_t e = messages->front();
 
+         // Test features - will be replaced by make a wish
+        if (game_master_mode == PLAY) {
+            if (e.event.key.code == sf::Keyboard::R && e.event.key.shift) {
+                std::cout << "Let it rain!\n";
+                for (int y=1; y<REGION_HEIGHT-1; ++y) {
+                    for (int x=1; x<REGION_WIDTH-1; ++x) {
+                        current_region->water_level[mapidx(x,y,REGION_DEPTH-2)] = 3;
+                    }
+                }
+            }
+        }
+
         // Global commands
         if (e.event.key.code == sf::Keyboard::Q) quitting = true;
         if (e.event.key.code == sf::Keyboard::F1) std::cout << ecs_profile_dump() << "\n";
@@ -146,7 +158,7 @@ void keyboard_system::update(const double ms) {
                 emit_deferred(map_dirty_message{});
                 emit_deferred(recalculate_mining_message{});
             }
-        }
+        }       
 	messages->pop();
 	}
 }
