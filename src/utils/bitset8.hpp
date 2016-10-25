67346d7cd109ed0338d1bfad1935cd21b42ca7e1
thebracket <herberticus@gmail.com>
2016-10-25 18:53:20 -0500

    Fix silly VS warning, enable VS multi-processor compilation.

--- src/utils/bitset8.hpp
+++ src/utils/bitset8.hpp
@@ -4,5 +4,5 @@ struct bitset8 {
 	uint8_t bits = 0;
 	void set(const uint8_t n) { bits|= n; }
 	void reset(const uint8_t n) { bits &= ~n; }
-	bool test(const uint8_t n) { return bits & n; }
+	bool test(const uint8_t n) { return (bits & n)!=0; }
 };
