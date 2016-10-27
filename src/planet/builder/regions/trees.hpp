af7919d2d404a94dd07ffd6dc71d7996f730861b
Herbert Wolverson <herberticus@gmail.com>
2016-10-27 14:23:10 -0500

    Region builder breakout - trees

---
+++ src/planet/builder/regions/trees.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../../../raws/raws.hpp"
+#include <rltk.hpp>
+#include "../../region.hpp"
+#include "../../planet.hpp"
+
+void build_trees(region_t &region, std::pair<biome_t, biome_type_t> &biome, rltk::random_number_generator &rng);
