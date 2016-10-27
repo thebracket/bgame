2ba8391cd3e2debb1d7de48a3ac84fd01abd5792
Herbert Wolverson <herberticus@gmail.com>
2016-10-27 13:39:21 -0500

    Start breaking out region_builder into chunks; starting point/crash site.

--- src/planet/builder/region_builder.cpp
+++ src/planet/builder/region_builder.cpp
@@ -20,49 +20,6 @@
 
 using namespace rltk;
 
-inline bool has_civilization(planet_t &planet, const int &x, const int &y) {
-    for (auto &town : planet.civs.settlements) {
-        if (town.world_x == x && town.world_y == y && town.status > 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-inline bool has_river(planet_t &planet, const int &x, const int &y) {
-    for (const river_t &river : planet.rivers) {
-        if (river.start_x == x && river.start_y == y) return true;
-        for (const river_step_t &s : river.steps) {
-            if (s.x == x && s.y == y) return true;
-        }
-    }
-    return false;
-}
-
-std::pair<int,int> builder_select_starting_region(planet_t &planet) {
-    std::pair<int,int> coords = std::make_pair(WORLD_WIDTH/2, WORLD_HEIGHT/2);
-
-    bool ok = false;
-    while (!ok) {
-        ok = true;
-        if (planet.landblocks[planet.idx(coords.first, coords.second)].type == block_type::WATER) ok = false;
-        if (planet.biomes[planet.landblocks[planet.idx(coords.first, coords.second)].biome_idx].mean_altitude < planet.water_height+2) ok = false;
-        if (!has_civilization(planet, coords.first, coords.second)) ok = false;
-        if (!has_river(planet, coords.first, coords.second)) ok = false;
-
-        if (!ok) {
-            --coords.first;
-            if (coords.first < 0) {
-                coords.first = WORLD_WIDTH-1;
-                --coords.second;
-            }
-        }        
-    }
-    std::cout << biome_defs[planet.biomes[planet.landblocks[planet.idx(coords.first, coords.second)].biome_idx].type].name << "\n";
-    
-    return coords;
-}
-
 inline std::pair<biome_t, biome_type_t> get_biome_for_region(planet_t &planet, const std::pair<int,int> &region) {
     const int idx = planet.idx(region.first, region.second);
     const int biome_idx = planet.landblocks[idx].biome_idx;
---
+++ src/planet/builder/regions/starting_point.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../../planet.hpp"
+
+std::pair<int,int> builder_select_starting_region(planet_t &planet);
