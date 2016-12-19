da9d1d23d421b2c7e1a887a6ea781153f5eef89d
Herbert Wolverson <herberticus@gmail.com>
2016-12-19 11:23:06 -0600

    Use RLTK's new mailbox system for grazer AI. Issue #75.

--- src/systems/ai/grazer_ai_system.cpp
+++ src/systems/ai/grazer_ai_system.cpp
@@ -1,10 +1,8 @@
 #include "grazer_ai_system.hpp"
-#include "../../messages/messages.hpp"
 #include "../../components/grazer_ai.hpp"
 #include "../../components/viewshed.hpp"
 #include "../../components/settler_ai.hpp"
 #include "../../components/sentient_ai.hpp"
-#include "../../components/renderable.hpp"
 #include "../../components/health.hpp"
 #include "../tasks/threat_scanner.hpp"
 
@@ -14,52 +12,42 @@ void grazer_ai_system::wander_randomly(entity_t &entity, position_t &original) {
     emit_deferred(entity_wants_to_move_randomly_message{entity.id});
 }
 
-void grazer_ai_system::configure() {
-    system_name = "Grazer AI System";
-    subscribe_mbox<tick_message>();
-}
+void grazer_ai_system::on_message(const tick_message &msg) {
+    parallel_each<grazer_ai, position_t, viewshed_t>([this] (entity_t &e, grazer_ai &ai, position_t &pos, viewshed_t &view) {
+        if (ai.initiative < 1) {
+            // Can we see anything scary?
+            auto hostile = tasks::can_see_hostile(e, pos, view, [] (entity_t &other) {
+                if (other.component<settler_ai_t>() || other.component<sentient_ai>()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
 
-void grazer_ai_system::update(const double ms) {
-    std::queue<tick_message> * tick = mbox<tick_message>();
-	while (!tick->empty()) {
-        tick->pop();
-
-        parallel_each<grazer_ai, position_t, viewshed_t>([this] (entity_t &e, grazer_ai &ai, position_t &pos, viewshed_t &view) {
-            if (ai.initiative < 1) {
-                // Can we see anything scary?
-                auto hostile = tasks::can_see_hostile(e, pos, view, [] (entity_t &other) {
-                  if (other.component<settler_ai_t>() || other.component<sentient_ai>()) {
-                      return true;
-                  } else {
-                      return false;
-                  }
-                });
-
-                if (!hostile.terrified) {
-                    // Handle the AI here
-                    const auto idx = mapidx(pos.x, pos.y, pos.z);
-                    if (current_region->tile_vegetation_type[idx] > 0) {
-                        if (rng.roll_dice(1,6)==1) emit_deferred(vegetation_damage_message{idx, 1});                         
-                    } else {
-                        this->wander_randomly(e, pos);
+            if (!hostile.terrified) {
+                // Handle the AI here
+                const auto idx = mapidx(pos.x, pos.y, pos.z);
+                if (current_region->tile_vegetation_type[idx] > 0) {
+                    if (rng.roll_dice(1,6)==1) emit_deferred(vegetation_damage_message{idx, 1});
+                } else {
+                    this->wander_randomly(e, pos);
+                }
+            } else {
+                // Poor creature is scared!
+                if (hostile.terror_distance < 1.5F) {
+                    // Attack the target
+                    auto health = entity(e.id)->component<health_t>();
+                    if (health) {
+                        if (!health->unconscious) emit_deferred(creature_attack_message{e.id, hostile.closest_fear});
                     }
                 } else {
-                    // Poor creature is scared!
-                    if (hostile.terror_distance < 1.5F) {
-                        // Attack the target
-                        auto health = entity(e.id)->component<health_t>();
-                        if (health) {
-                            if (!health->unconscious) emit_deferred(creature_attack_message{e.id, hostile.closest_fear});
-                        }
-                    } else {
-                        emit_deferred(entity_wants_to_flee_message{e.id, hostile.closest_fear});
-                    }
+                    emit_deferred(entity_wants_to_flee_message{e.id, hostile.closest_fear});
                 }
-
-                ai.initiative = std::max(1, rng.roll_dice(1, 12) - ai.initiative_modifier);
-            } else {
-                --ai.initiative;
             }
-        });
-    }
+
+            ai.initiative = std::max(1, rng.roll_dice(1, 12) - ai.initiative_modifier);
+        } else {
+            --ai.initiative;
+        }
+    });
 }
--- src/systems/ai/grazer_ai_system.hpp
+++ src/systems/ai/grazer_ai_system.hpp
@@ -2,11 +2,11 @@
 
 #include <rltk.hpp>
 #include "../components/position.hpp"
+#include "../../messages/messages.hpp"
 
-class grazer_ai_system : public rltk::base_system {
+class grazer_ai_system : public rltk::mailbox_system<tick_message> {
 public:
-	virtual void update(const double duration_ms) override final;
-	virtual void configure() override final;
-private:
+    grazer_ai_system() { system_name = "Grazer AI"; }
+	virtual void on_message(const tick_message &msg) override final;
     void wander_randomly(rltk::entity_t &entity, position_t &original);
 };
