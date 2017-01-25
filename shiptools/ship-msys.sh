e874cc9fc7a59ebca7b73203ab49447a54f6cab6
Herbert Wolverson <herberticus@gmail.com>
2017-01-25 13:34:21 -0600

    Msys build works

--- shiptools/ship-msys.sh
+++ shiptools/ship-msys.sh
@@ -35,20 +35,34 @@ cp $GAMEBASE/rex/* rex
 echo "5 - copying dependencies"
 cp /mingw64/bin/sfml*.dll .
 cp /mingw64/bin/libwinpthread-1.dll .
-
+cp /mingw64/bin/libstdc++-6.dll .
+cp /mingw64/bin/libgcc_s_seh-1.dll .
+cp /mingw64/bin/libjpeg-8.dll .
+cp /mingw64/bin/libfreetype-6.dll .
+cp /mingw64/bin/libgomp-1.dll .
+cp /mingw64/bin/lua51.dll .
+cp /mingw64/bin/libbz2-1.dll .
+cp /mingw64/bin/libharfbuzz-0.dll .
+cp /mingw64/bin/zlib1.dll .
+cp /mingw64/bin/libpng16-16.dll .
+cp /mingw64/bin/libglib-2.0-0.dll .
+cp /mingw64/bin/libgraphite2.dll .
+cp /mingw64/bin/libintl-8.dll .
+cp /mingw64/bin/libpcre-1.dll .
+cp /mingw64/bin/libiconv-2.dll .
 
 echo "4 - stripping binaries"
 strip $tempfolder/bgame.exe
 
 #echo "5 - making tarball: ${tarball}"
-#tar cvfz $tarball *
+tar cvfz $tarball *
 
 popd
 popd
 
 #echo "6 - cleaning up folder"
-#rm -rf $tempfolder
+rm -rf $tempfolder
 
 #echo "7 - publishing the tarball"
-#mv $tarball /var/www/bfnightly
+scp $tarball herbert@172.16.10.193:/var/www/bfnightly
 
