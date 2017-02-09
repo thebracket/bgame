0c722cbcf175777eeb8eab2c35ae87ae9699b165
Herbert Wolverson <herberticus@gmail.com>
2017-02-09 11:48:12 -0600

    Show 'new arrival' as status on new arrivals.

--- src/systems/ai/ai_new_arrival.cpp
+++ src/systems/ai/ai_new_arrival.cpp
@@ -5,6 +5,7 @@
 #include "../../components/ai_tag_my_turn.hpp"
 #include "../../messages/log_message.hpp"
 #include "../../raws/string_table.hpp"
+#include "../../components/settler_ai.hpp"
 
 void ai_new_arrival::configure() {}
 
@@ -13,6 +14,11 @@ void ai_new_arrival::update(const double duration_ms) {
 
     each<ai_tag_my_turn_t, ai_settler_new_arrival_t>([] (entity_t &e, ai_tag_my_turn_t &t, ai_settler_new_arrival_t &arrival) {
         delete_component<ai_tag_my_turn_t>(e.id); // No more turns
+
+        auto settler = e.component<settler_ai_t>();
+        if (settler) {
+            settler->job_status = "New Arrival";
+        }
 
         if (arrival.turns_since_arrival == 0 && rng.roll_dice(1,6)==1) {
             const std::string quip = new_arrival_quips.random_entry(rng);
