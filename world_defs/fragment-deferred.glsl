803b7a8de6022af2e76a3814051768a373b7e94c
Herbert Wolverson <herberticus@gmail.com>
2017-03-21 12:39:03 -0500

    Lighting is per pixel.

--- world_defs/fragment-deferred.glsl
+++ world_defs/fragment-deferred.glsl
@@ -2,11 +2,20 @@
 
 uniform sampler2D my_color_texture;
 varying vec3 normal;
+varying vec3 ambient;
+varying vec4 diffuse;
+varying vec3 lightDir;
 
 // Outdoor rendering
 void main()
 {
-    //gl_FragColor = vec4(texture2D(my_color_texture, gl_TexCoord[0].st) * gl_Color);;
-    gl_FragColor = texture2D(my_color_texture, gl_TexCoord[0].st) * gl_Color;
-    //gl_FragColor = gl_Color;
+    float NdotL = max(dot(normal, lightDir), 0.0);
+
+    vec4 color = vec4(
+         (diffuse.r * NdotL) + ambient.r,
+         (diffuse.g * NdotL) + ambient.g,
+         (diffuse.b * NdotL) + ambient.b,
+         1.0);
+
+    gl_FragColor = texture2D(my_color_texture, gl_TexCoord[0].st) * color * gl_Color;
 }
--- world_defs/vertex-deferred.glsl
+++ world_defs/vertex-deferred.glsl
@@ -3,7 +3,9 @@
 uniform vec3 light_position;
 uniform vec3 light_ambient;
 uniform vec3 light_diffuse;
-//varying vec3 normal;
+varying vec3 normal, ambient;
+varying vec4 diffuse;
+varying vec3 lightDir;
 
 // Outdoor rendering
 void main()
@@ -12,25 +14,26 @@ void main()
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 
     // Transform the normal and normalize (heh) it
-    vec3 normal = normalize(gl_Normal * gl_NormalMatrix);
+    normal = normalize(gl_Normal * gl_NormalMatrix);
 
     // Now we get the light direction (in eye space)
     vec3 light_position_transformed = light_position * gl_NormalMatrix;
-    vec3 lightDir = normalize((vec3(gl_Vertex) * gl_NormalMatrix)-light_position_transformed);
+    lightDir = normalize((vec3(gl_Vertex) * gl_NormalMatrix)-light_position_transformed);
 
     // Now we get the dot product between the light and the vertex
-    float NdotL = max(dot(normal, lightDir), 0.0);
-    vec4 diffuse = vec4(light_diffuse, 1.0f) * gl_Color;
+    diffuse = vec4(light_diffuse, 1.0f) * gl_Color;
 
     // Pass the color through because we use it for colored textures
     //gl_FrontColor = vec4(NdotL, NdotL, NdotL, 1.0);
     //gl_FrontColor = vec4(normal.x, normal.y, normal.z, 1.0);
 
-    gl_FrontColor = vec4(
+    /*gl_FrontColor = vec4(
                 (diffuse.r * NdotL) + light_ambient.r,
                 (diffuse.g * NdotL) + light_ambient.g,
                 (diffuse.b * NdotL) + light_ambient.b,
-                1.0);
+                1.0);*/
+    gl_FrontColor = gl_Color;
+    ambient = light_ambient;
 
     // Project the texture
     gl_TexCoord[0] = gl_MultiTexCoord0;
