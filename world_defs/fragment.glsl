352b631f2fb8a4c5b72b320a896ccc84b7aa0276
Herbert Wolverson <herberticus@gmail.com>
2017-04-01 09:08:23 -0500

    Remove unused shader files.

--- world_defs/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 120
-
-uniform sampler2D my_color_texture;
-
-void main()
-{
-    // Sampling The Texture And Passing It To The Frame Buffer
-    gl_FragColor = texture2D(my_color_texture, gl_TexCoord[0].xy) * gl_Color;
-}
