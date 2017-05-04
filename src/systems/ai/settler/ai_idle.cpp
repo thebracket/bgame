ff7217773fefa2f6d3f30f54b0a509fd07de29e7
Herbert Wolverson <herberticus@gmail.com>
2017-05-04 15:53:45 -0500

    Sentients and grazers can move once more.

--- src/systems/ai/settler/ai_idle.cpp
+++ src/systems/ai/settler/ai_idle.cpp
@@ -15,7 +15,7 @@
 
 void ai_idle::configure() {}
 
-void idle_grazer(entity_t &e, ai_tag_my_turn_t &t, ai_mode_idle_t &idle, grazer_ai * grazer) {
+void idle_grazer(entity_t &e, ai_tag_my_turn_t &t, grazer_ai &grazer) {
     auto pos = e.component<position_t>();
     delete_component<ai_tag_my_turn_t>(e.id);
 
@@ -24,56 +24,48 @@ void idle_grazer(entity_t &e, ai_tag_my_turn_t &t, ai_mode_idle_t &idle, grazer_
     if (current_region->tile_vegetation_type[idx] > 0) {
         if (rng.roll_dice(1,6)==1) emit_deferred(vegetation_damage_message{idx, 1});
     } else {
-        emit_deferred(entity_wants_to_move_randomly_message{e.id});
-        emit_deferred(huntable_moved_message{});
+        emit(entity_wants_to_move_randomly_message{e.id});
+        emit(huntable_moved_message{});
     }
     delete_component<ai_tag_my_turn_t>(e.id);
 }
 
-void idle_sentient(entity_t &e, ai_tag_my_turn_t &t, ai_mode_idle_t &idle, sentient_ai * sentient) {
+void idle_sentient(entity_t &e, ai_tag_my_turn_t &t, sentient_ai &sentient) {
     auto pos = e.component<position_t>();
-    int feelings = planet.civs.civs[sentient->civ_id].cordex_feelings;
+    int feelings = planet.civs.civs[sentient.civ_id].cordex_feelings;
 
-    if (sentient->hostile || feelings < 0) {
-        sentient->goal = SENTIENT_GOAL_KILL;
+    if (sentient.hostile || feelings < 0) {
+        sentient.goal = SENTIENT_GOAL_KILL;
     } else {
-        sentient->goal = SENTIENT_GOAL_IDLE;
+        sentient.goal = SENTIENT_GOAL_IDLE;
     }
 
-    if (sentient->goal == SENTIENT_GOAL_KILL) {
+    if (sentient.goal == SENTIENT_GOAL_KILL) {
         //std::cout << "Sentient kill mode\n";
         // Close for the kill!
         const int idx = mapidx(*pos);
         if (settler_map.get(idx) < MAX_DIJSTRA_DISTANCE-1) {
             position_t destination = settler_map.find_destination(*pos);
-            emit_deferred(entity_wants_to_move_message{e.id, destination});
-            emit_deferred(renderables_changed_message{});
+            emit(entity_wants_to_move_message{e.id, destination});
+            emit(renderables_changed_message{});
         } else {
-            sentient->goal = SENTIENT_GOAL_IDLE;
+            sentient.goal = SENTIENT_GOAL_IDLE;
         }
     } else {
         // Wander aimlessly
-        emit_deferred(entity_wants_to_move_randomly_message{e.id});
+        emit(entity_wants_to_move_randomly_message{e.id});
     }
     delete_component<ai_tag_my_turn_t>(e.id);
 }
 
-void idle_iterator(entity_t &e, ai_tag_my_turn_t &t, ai_mode_idle_t &idle) {
-    auto settler = e.component<settler_ai_t>();
-    auto sentient = e.component<sentient_ai>();
-    auto grazer = e.component<grazer_ai>();
-
-    if (grazer) {
-        idle_grazer(e, t, idle, grazer);
-    } else if (sentient) {
-        idle_sentient(e, t, idle, sentient);
-    } else if (settler) {
-        // TODO
-    }
+void idle_iterator(entity_t &e, ai_tag_my_turn_t &t, ai_mode_idle_t &idle, settler_ai_t &settler) {
+    // Nothing for settlers to do, currently.
 }
 
 void ai_idle::update(const double duration_ms) {
     if (pause_mode != RUNNING) return;
 
-    each<ai_tag_my_turn_t, ai_mode_idle_t>(idle_iterator);
+    each<ai_tag_my_turn_t, ai_mode_idle_t, settler_ai_t>(idle_iterator);
+    each<ai_tag_my_turn_t, sentient_ai>(idle_sentient);
+    each<ai_tag_my_turn_t, grazer_ai>(idle_grazer);
 }
--- src/systems/ai/settler/ai_status_effects.cpp
+++ src/systems/ai/settler/ai_status_effects.cpp
@@ -11,6 +11,7 @@ void ai_status_effects::update(const double duration_ms) {
     each<health_t, ai_tag_my_turn_t>([] (entity_t &e, health_t &health, ai_tag_my_turn_t &t) {
         // If unsconscious, cancel the turn
         if (health.unconscious) {
+            std::cout << "Cancelling turn - unconcscious\n";
             delete_component<ai_tag_my_turn_t>(e.id);
         }
     });
