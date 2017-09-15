f79420ee1097cdb6a35927551f1721d4be160674
Herbert Wolverson <herberticus@gmail.com>
2017-09-15 13:48:09 -0500

    Readback world position from mouse texture.

--- src/render_engine/render_engine.hpp
+++ src/render_engine/render_engine.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <memory>
+#include "fbo/gbuffer.hpp"
+
 namespace render {
     extern bool camera_moved;
     extern bool models_changed;
     void render_gl(const double &duration_ms);
+	extern std::unique_ptr<gbuffer_t> gbuffer;
 }
--- src/systems/mouse.cpp
+++ src/systems/mouse.cpp
@@ -3,6 +3,7 @@
 #include "../bengine/imgui_impl_glfw_gl3.h"
 #include "../bengine/gl_include.hpp"
 #include "../bengine/main_window.hpp"
+#include "../render_engine/render_engine.hpp"
 
 namespace systems {
 	int mouse_x = 0;
@@ -12,6 +13,23 @@ namespace systems {
 	bool middle_click = false;
 	bool wheel_up = false;
 	bool wheel_down = false;
+
+	int mouse_wx = 0;
+	int mouse_wy = 0;
+	int mouse_wz = 0;
+
+	void read_gl_position(const int &screen_height) {
+		glBindFramebuffer(GL_FRAMEBUFFER, render::gbuffer->fbo_id);
+		float pixels[3];
+		glReadPixels(mouse_x, screen_height - mouse_y, 1, 1, GL_RGB, GL_FLOAT, &pixels[0]);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		mouse_wx = pixels[0] + 0.5f;
+		mouse_wz = pixels[1];
+		mouse_wy = pixels[2] + 0.5f;
+
+		//std::cout << mouse_wx << " / " << mouse_wy << " / " << mouse_wz << "\n";
+	}
 
 	void poll_mouse() {
 		int screen_w, screen_h;
@@ -29,6 +47,7 @@ namespace systems {
 			wheel_down = false;
 			if (wheel < 0) wheel_up = true;
 			if (wheel > 0) wheel_down = true;
+			read_gl_position(screen_h);
 		}
 		else {
 			left_click = false;
