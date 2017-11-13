8f3feecb290c0cea81391a91c1c2ee858c59a3fc
Herbert Wolverson <herberticus@gmail.com>
2017-11-13 12:57:07 -0600

    Request OpenGL 4.5 context.

--- src/bengine/main_window.cpp
+++ src/bengine/main_window.cpp
@@ -22,8 +22,8 @@ namespace bengine {
     }
 
     void init_glfw_window_hints() {
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     }
