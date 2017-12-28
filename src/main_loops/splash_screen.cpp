be7fcf0947be3bb850120d795a8ea56feecc5f4c
Herbert Wolverson <herberticus@gmail.com>
2017-12-28 13:42:19 -0600

    Mostly working on msys2.

--- src/main_loops/splash_screen.cpp
+++ src/main_loops/splash_screen.cpp
@@ -57,6 +57,7 @@ namespace splash_screen {
         cursor_shader = load_shaders("game_assets/highlight_vertex.glsl", "game_assets/highlight_fragment.glsl");
         particle_shader = load_shaders("game_assets/particle_vertex.glsl", "game_assets/particle_fragment.glsl");
         dirlight_shader = load_shaders("game_assets/dirlight_vertex.glsl", "game_assets/dirlight_fragment.glsl");
+        lighter_shader = load_shaders("game_assets/lighter_vertex.glsl", "game_assets/lighter_fragment.glsl");
     }
 
     void init_raws(int id) {
