5060da4c6ea30c4d34dd9ddce56c4f0e5c7d76b2
Herbert Wolverson <herberticus@gmail.com>
2018-01-30 09:57:17 -0600

    Add itch.io push script.

---
+++ bundle-alpha.bat
@@ -0,0 +1,26 @@
+@echo Building Pre-Alpha Release
+@md c:\nfbuild > nul 2> nul
+@SET bundleDir=c:\nfbuild\NoxFutura-%DATE:~-4%-%DATE:~4,2%-%DATE:~7,2%
+@md %bundleDir%  > nul 2> nul
+@md %bundleDir%\game_assets > nul 2> nul
+@md %bundleDir%\game_assets\cursors > nul 2> nul
+@md %bundleDir%\game_assets\rex > nul 2> nul
+@md %bundleDir%\game_assets\terrain > nul 2> nul
+@md %bundleDir%\game_assets\vox > nul 2> nul
+@md %bundleDir%\game_assets\worldgen > nul 2> nul
+@md %bundleDir%\world_defs > nul 2> nul
+@SET exeDirectory=NoxFuturaVS\x64\Release\
+@copy %exeDirectory%*.dll %bundleDir% > nul 2> nul
+@copy %exeDirectory%NoxFuturaVS.exe %bundleDir%\NoxFutura.exe > nul 2> nul
+@copy game_assets\*.* %bundleDir%\game_assets > nul 2> nul
+@copy game_assets\cursors\*.* %bundleDir%\game_assets\cursors > nul 2> nul
+@copy game_assets\rex\*.* %bundleDir%\game_assets\rex > nul 2> nul
+@copy game_assets\terrain\*.* %bundleDir%\game_assets\terrain > nul 2> nul
+@copy game_assets\vox\*.* %bundleDir%\game_assets\vox > nul 2> nul
+@copy game_assets\worldgen\*.* %bundleDir%\game_assets\worldgen > nul 2> nul
+@copy world_defs\*.* %bundleDir%\world_defs > nul 2> nul
+@echo Copied Bundle
+@SET butler=%APPDATA%\itch\bin\butler.exe
+@%butler% upgrade
+@SET uploadCommand=%butler% push %bundleDir% thebracket/nox-futura:windows-pre-alpha
+@%uploadCommand%
