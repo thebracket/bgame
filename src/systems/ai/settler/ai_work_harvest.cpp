2d29edcc9062bdec2876bce02c53e1c91206acbc
Herbert Wolverson <herberticus@gmail.com>
2018-02-06 14:35:20 -0600

    Harvesting uses the improved template.

--- src/systems/ai/settler/ai_work_harvest.cpp
+++ src/systems/ai/settler/ai_work_harvest.cpp
@@ -1,5 +1,5 @@
-#include "jobs_board.hpp"
-#include "ai_work_template.hpp"
+#include "ai_work_harvest.hpp"
+#include "templated_work_steps_t.hpp"
 #include "../../../components/position.hpp"
 #include "../../../components/ai_tags/ai_tag_work_harvest.hpp"
 #include "../distance_map_system.hpp"
@@ -19,6 +19,10 @@
 #include "../../damage/damage_system.hpp"
 #include "../../../global_assets/farming_designations.hpp"
 #include "../../../components/item_tags/item_seed_t.hpp"
+#include "../../../raws/defs/item_def_t.hpp"
+#include "../../../raws/raws.hpp"
+#include "../../../raws/materials.hpp"
+#include "../../../raws/defs/material_def_t.hpp"
 
 namespace systems {
 	namespace ai_harvest {
@@ -38,189 +42,153 @@ namespace systems {
 			}
 		}
 
-		static bool first_run = true;
+		static const char * job_tag = "Farm - Harvest";
+		static work::templated_work_steps_t<ai_tag_work_harvest> work;
+
+		inline void find_hoe(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work::get_tool<item_farming_t, designated_farmer_t, ai_tag_work_harvest>(e, work, h, pos,
+				[&h]() {
+				// On we have it
+				h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
+			},
+				[&h]() {
+				// On need to find it
+				h.step = ai_tag_work_harvest::harvest_steps::FETCH_HOE;
+			});
+		}
+
+		inline void fetch_hoe(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work.follow_path(h, pos, e, [&h]() {
+				// Cancel
+				h.current_path.reset();
+				h.step = ai_tag_work_harvest::harvest_steps::FIND_HOE;
+				return;
+			}, [&h, &e]() {
+				// Success
+				h.current_path.reset();
+				h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
+
+				inventory_system::pickup_item(h.tool_id, e.id);
+				return;
+			});
+		}
+
+		inline void find_harvest(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			std::map<int, position_t> harvest_targets;
+			for (const auto ht : farm_designations->harvest) {
+				const float distance = bengine::distance3d(pos.x, pos.y, pos.z, ht.second.x, ht.second.y, ht.second.z);
+				harvest_targets.insert(std::make_pair(static_cast<int>(distance), ht.second));
+			}
+			h.current_path.reset();
+			bool done = false;
+			auto iterator = harvest_targets.begin();
+			while (!done && !h.current_path) {
+				h.current_path = find_path(pos, iterator->second);
+				if (!h.current_path->success) {
+					++iterator;
+					if (iterator == harvest_targets.end()) done = true;
+				}
+				else {
+					h.step = ai_tag_work_harvest::harvest_steps::GOTO_HARVEST;
+					return;
+				}
+			}
+			if (!h.current_path) {
+				work.cancel_work_tag(e);
+				return;
+			}
+		}
+
+		inline void goto_harvest(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work.follow_path(h, pos, e, [&h]() {
+				// Cancel
+				h.current_path.reset();
+				h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
+				return;
+			}, [&h, &e]() {
+				// Success
+				h.current_path.reset();
+				h.step = ai_tag_work_harvest::harvest_steps::DO_HARVEST;
+				return;
+			});
+			return;
+		}
+
+		inline void do_harvest(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			const int idx = mapidx(pos);
+			farm_designations->harvest.erase(std::remove_if(
+				farm_designations->harvest.begin(),
+				farm_designations->harvest.end(),
+				[&idx](std::pair<bool, position_t> p) { return idx == mapidx(p.second); }
+			),
+				farm_designations->harvest.end());
+
+			if (work::skill_check_or_damage<ai_tag_work_harvest>(e, "Farming", DIFFICULTY_EASY, work, 1, "Farming Accident")) {
+				auto farm_finder = farm_designations->farms.find(idx);
+
+				// Create the harvesting result
+				if (veg_type(idx) == 0) {
+					work.cancel_work_tag(e);
+					return;
+				}
+				std::string cname = "";
+				auto name = e.component<name_t>();
+				if (name) cname = name->first_name + std::string(" ") + name->last_name;
+				const auto plant = get_plant_def(veg_type(idx));
+				const std::string result = plant->provides[veg_lifecycle(idx)];
+				if (result != "none") {
+					std::string mat_type = "organic";
+					auto item_finder = get_item_def(result);
+					if (item_finder != nullptr) {
+						if (item_finder->categories.test(ITEM_FOOD)) mat_type = "food";
+						if (item_finder->categories.test(ITEM_SPICE)) mat_type = "spice";
+					}
+					int n_spawn = 1;
+					if (farm_finder != farm_designations->farms.end()) {
+						const int n = farm_finder->second.fertilized ? 4 : 2;
+						n_spawn = rng.roll_dice(n, 4);
+						farm_finder->second.state = farm_steps::GROWING;
+						farm_finder->second.fertilized = false;
+
+					}
+					for (int i = 0; i < n_spawn; ++i) {
+						auto item = spawn_item_on_ground_ret(pos.x, pos.y, pos.z, result, get_material_by_tag(mat_type), 3, 100, e.id, cname);
+						item->component<item_t>()->item_name = plant->name;
+					}
+				}
+
+				// Spawn seeds
+				const int n_seeds = rng.roll_dice(1, 4) + 1;
+				for (int i = 0; i < n_seeds; ++i) {
+					auto item = spawn_item_on_ground_ret(pos.x, pos.y, pos.z, "seed", get_material_by_tag("organic"), 3, 100, e.id, cname);
+					item->component<item_t>()->item_name = plant->name + std::string(" Seed");
+					item->component<item_seed_t>()->grows_into = plant->tag;
+				}
+
+				// Knock tile back to germination
+				set_veg_lifecycle(idx, 0);
+				set_veg_ticker(idx, 0);
+				chunks::mark_chunk_dirty_by_tileidx(idx);
+
+				// Become idle - done
+				call_home("AI", "Harvest", result);
+				work.cancel_work_tag(e);
+				h.step = ai_tag_work_harvest::harvest_steps::FIND_HOE;
+			}
+		}
+
+		void dispatch(entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
+			switch (h.step) {
+			case ai_tag_work_harvest::harvest_steps::FIND_HOE: find_hoe(e, h, t, pos); break;
+			case ai_tag_work_harvest::harvest_steps::FETCH_HOE: fetch_hoe(e, h, t, pos); break;
+			case ai_tag_work_harvest::harvest_steps::FIND_HARVEST: find_harvest(e, h, t, pos); break;
+			case ai_tag_work_harvest::harvest_steps::GOTO_HARVEST: goto_harvest(e, h, t, pos); break;
+			case ai_tag_work_harvest::harvest_steps::DO_HARVEST: do_harvest(e, h, t, pos); break;
+			}
+		}
 
 		void run(const double &duration_ms) {
-			if (first_run) {
-				first_run = false;
-				register_job_offer<ai_tag_work_harvest>(jobs_board::evaluate_harvest);
-			}
-
-			ai_work_template<ai_tag_work_harvest> work;
-			work.do_ai("Harvesting", [&work](entity_t &e, ai_tag_work_harvest &h, ai_tag_my_turn_t &t, position_t &pos) {
-				if (h.step == ai_tag_work_harvest::harvest_steps::FIND_HOE) {
-					// Do I already have a pick?
-					bool have_tool = false;
-					each<item_t, item_carried_t, claimed_t, item_farming_t>([&e, &have_tool](entity_t &E, item_t &i, item_carried_t &ic, claimed_t &claimed, item_farming_t &farm) {
-						if (ic.carried_by == e.id) have_tool = true;
-					});
-					if (have_tool) {
-						std::cout << "Skipping get pick - we already have one!\n";
-						h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
-						return;
-					}
-					else {
-						// We need to fetch the pick
-						std::size_t tool_id = -1;
-						each<item_t, claimed_t, item_farming_t>([&e, &tool_id](entity_t &E, item_t &i, claimed_t &claimed, item_farming_t &farm) {
-							if (claimed.claimed_by == e.id) tool_id = E.id;
-						});
-						if (tool_id == -1) {
-							std::cout << "Unable to find claimed tool. Bug!\n";
-							work.cancel_work_tag(e);
-							return;
-						}
-
-						h.tool_id = tool_id;
-						auto tool_pos = inventory::get_item_location(tool_id);
-						if (tool_pos) {
-							h.current_path = find_path(pos, *tool_pos);
-							if (h.current_path->success) {
-								h.step = ai_tag_work_harvest::harvest_steps::FETCH_HOE;
-							}
-							else {
-								work.cancel_work_tag(e);
-								return;
-							}
-						}
-						else {
-							work.cancel_work_tag(e);
-							return;
-						}
-					}
-				}
-				else if (h.step == ai_tag_work_harvest::harvest_steps::FETCH_HOE) {
-					work.follow_path(h, pos, e, [&h]() {
-						// Cancel
-						h.current_path.reset();
-						h.step = ai_tag_work_harvest::harvest_steps::FIND_HOE;
-						return;
-					}, [&h, &e]() {
-						// Success
-						h.current_path.reset();
-						h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
-
-						inventory_system::pickup_item(h.tool_id, e.id);
-						return;
-					});
-					return;
-				} 
-				else if (h.step == ai_tag_work_harvest::harvest_steps::FIND_HARVEST) {
-					std::map<int, position_t> harvest_targets;
-					for (const auto ht : farm_designations->harvest) {
-						const float distance = bengine::distance3d(pos.x, pos.y, pos.z, ht.second.x, ht.second.y, ht.second.z);
-						harvest_targets.insert(std::make_pair(static_cast<int>(distance), ht.second));
-					}
-					h.current_path.reset();
-					bool done = false;
-					auto iterator = harvest_targets.begin();
-					while (!done && !h.current_path) {
-						h.current_path = find_path(pos, iterator->second);
-						if (!h.current_path->success) {
-							++iterator;
-							if (iterator == harvest_targets.end()) done = true;
-						}
-						else {
-							h.step = ai_tag_work_harvest::harvest_steps::GOTO_HARVEST;
-							return;
-						}
-					}
-					if (!h.current_path) {
-						work.cancel_work_tag(e);
-						return;
-					}
-					return;
-				}
-				else if (h.step == ai_tag_work_harvest::harvest_steps::GOTO_HARVEST) {
-					work.follow_path(h, pos, e, [&h]() {
-						// Cancel
-						h.current_path.reset();
-						h.step = ai_tag_work_harvest::harvest_steps::FIND_HARVEST;
-						return;
-					}, [&h, &e]() {
-						// Success
-						h.current_path.reset();
-						h.step = ai_tag_work_harvest::harvest_steps::DO_HARVEST;
-						return;
-					});
-					return;
-				}
-				else if (h.step == ai_tag_work_harvest::harvest_steps::DO_HARVEST) {
-					const int idx = mapidx(pos);
-					farm_designations->harvest.erase(std::remove_if(
-						farm_designations->harvest.begin(),
-						farm_designations->harvest.end(),
-						[&idx](std::pair<bool, position_t> p) { return idx == mapidx(p.second); }
-					),
-						farm_designations->harvest.end());
-
-					auto stats = e.component<game_stats_t>();
-					if (!stats) {
-						std::cout << "Cancelled because of missing stats.\n";
-						work.cancel_work_tag(e);
-						return;
-					}
-
-					auto skill_check = skill_roll(e.id, *stats, rng, "Farming", DIFFICULTY_EASY);
-					if (skill_check >= SUCCESS) {
-						auto farm_finder = farm_designations->farms.find(idx);
-
-						// Create the harvesting result
-						if (veg_type(idx) == 0) {
-							work.cancel_work_tag(e);
-							return;
-						}
-						std::string cname = "";
-						auto name = e.component<name_t>();
-						if (name) cname = name->first_name + std::string(" ") + name->last_name;
-						const auto plant = get_plant_def(veg_type(idx));
-						const std::string result = plant->provides[veg_lifecycle(idx)];
-						if (result != "none") {
-							std::string mat_type = "organic";
-							auto item_finder = get_item_def(result);
-							if (item_finder != nullptr) {
-								if (item_finder->categories.test(ITEM_FOOD)) mat_type = "food";
-								if (item_finder->categories.test(ITEM_SPICE)) mat_type = "spice";
-							}
-							int n_spawn = 1;
-							if (farm_finder != farm_designations->farms.end()) {
-								const int n = farm_finder->second.fertilized ? 4 : 2;
-								n_spawn = rng.roll_dice(n, 4);
-								farm_finder->second.state = farm_steps::GROWING;
-								farm_finder->second.fertilized = false;
-
-							}
-							for (int i = 0; i < n_spawn; ++i) {
-								auto item = spawn_item_on_ground_ret(pos.x, pos.y, pos.z, result, get_material_by_tag(mat_type), 3, 100, e.id, cname);
-								item->component<item_t>()->item_name = plant->name;
-							}
-						}
-
-						// Spawn seeds
-						const int n_seeds = rng.roll_dice(1, 4)+1;
-						for (int i = 0; i < n_seeds; ++i) {
-							auto item = spawn_item_on_ground_ret(pos.x, pos.y, pos.z, "seed", get_material_by_tag("organic"), 3, 100, e.id, cname);
-							item->component<item_t>()->item_name = plant->name + std::string(" Seed");
-							item->component<item_seed_t>()->grows_into = plant->tag;
-						}
-
-						// Knock tile back to germination
-						set_veg_lifecycle(idx, 0);
-						set_veg_ticker(idx, 0);
-						chunks::mark_chunk_dirty_by_tileidx(idx);
-
-						// Become idle - done
-						call_home("AI", "Harvest", result);
-						work.cancel_work_tag(e);
-						h.step = ai_tag_work_harvest::harvest_steps::FIND_HOE;
-						return;
-					}
-					else {
-						if (skill_check == CRITICAL_FAIL) damage_system::inflict_damage(damage_system::inflict_damage_message{ e.id, 1, "Farming Accident" });
-						return;
-					}
-				}
-			});
+			work.do_work(jobs_board::evaluate_harvest, dispatch, job_tag);
 		}
 	}
 }
