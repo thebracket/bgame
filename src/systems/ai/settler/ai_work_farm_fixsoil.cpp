9feee9327fc949e9bcd9716a2e7b31f283838719
Herbert Wolverson <herberticus@gmail.com>
2018-02-07 10:10:19 -0600

    Use new template for soil fixing.

--- src/systems/ai/settler/ai_work_farm_fixsoil.cpp
+++ src/systems/ai/settler/ai_work_farm_fixsoil.cpp
@@ -1,6 +1,5 @@
-#include "jobs_board.hpp"
-#include "ai_work_template.hpp"
 #include "ai_work_farm_fixsoil.hpp"
+#include "templated_work_steps_t.hpp"
 #include "../../../components/ai_tags/ai_tag_work_farm_fixsoil.hpp"
 #include "../../../components/farming/designated_farmer.hpp"
 #include "../../../global_assets/farming_designations.hpp"
@@ -16,12 +15,10 @@ namespace systems {
 	namespace ai_farm_fixsoil {
 		using namespace bengine;
 		using namespace jobs_board;
-		using namespace distance_map;
-		using namespace dijkstra;
 		using namespace region;
 
 		namespace jobs_board {
-			void evaluate_farm_clearance(job_board_t &board, entity_t &e, position_t &pos, job_evaluator_base_t *jt) {
+			void evaluate_farm_fixing(job_board_t &board, entity_t &e, position_t &pos, job_evaluator_base_t *jt) {
 				if (e.component<designated_farmer_t>() == nullptr) return; // Not a farmer
 				if (farm_designations->farms.empty()) return; // There are no farms
 				if (!inventory::is_item_category_available<item_topsoil_t>()) return; // No available soil
@@ -34,213 +31,177 @@ namespace systems {
 			}
 		}
 
-		static bool first_run = true;
+		static const char * job_tag = "Farm - Planting";
+		static work::templated_work_steps_t<ai_tag_work_farm_fixsoil> work;
 
-		void run(const double &duration_ms) {
-			if (first_run) {
-				first_run = false;
-				register_job_offer<ai_tag_work_farm_fixsoil>(jobs_board::evaluate_farm_clearance);
+		inline void find_hoe(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work::get_tool<item_farming_t, designated_farmer_t, ai_tag_work_farm_fixsoil>(e, work, h, pos,
+				[&h]() {
+				// On we have it
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL;
+			},
+				[&h]() {
+				// On need to find it
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FETCH_HOE;
+			});
+		}
+
+		inline void fetch_hoe(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work.follow_path(h, pos, e, [&h]() {
+				// Cancel
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE;
+				return;
+			}, [&h, &e]() {
+				// Success
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL;
+
+				inventory_system::pickup_item(h.tool_id, e.id);
+				return;
+			});
+		}
+
+		inline void find_soil(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			if (!inventory::is_item_category_available<item_topsoil_t>()) {
+				// No soil available - bail out
+				work.cancel_work_tag(e);
+				return;
+			}
+			auto soil_id = inventory::find_closest_unclaimed_item_by_category_and_claim_it_immediately<item_topsoil_t>(e.id, pos);
+			h.soil_id = soil_id;
+			if (soil_id == 0) {
+				// No soil available - bail out
+				work.cancel_work_tag(e);
+				return;
+			}
+			auto soil_position = inventory::get_item_location(soil_id);
+			if (!soil_position) {
+				// No soil available - bail out
+				work.cancel_work_tag(e);
+				return;
+			}
+			h.current_path.reset();
+			h.current_path = find_path(pos, *soil_position);
+			if (!h.current_path->success) {
+				// No soil available - bail out
+				work.cancel_work_tag(e);
+				return;
+			}
+			h.step = ai_tag_work_farm_fixsoil::fix_steps::FETCH_SOIL;
+		}
+
+		inline void fetch_soil(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work.follow_path(h, pos, e, [&h]() {
+				// Cancel
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL;
+				return;
+			}, [&h, &e]() {
+				// Success
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET;
+
+				inventory_system::pickup_item(h.soil_id, e.id);
+				return;
+			});
+			return;
+		}
+
+		inline void find_target(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			std::map<int, position_t> clear_targets;
+			for (const auto &f : farm_designations->farms) {
+				if (f.second.state == farm_steps::FIX_SOIL) {
+					auto[X, Y, Z] = idxmap(f.first);
+					const float distance = bengine::distance3d(pos.x, pos.y, pos.z, X, Y, Z);
+					clear_targets.insert(std::make_pair(static_cast<int>(distance), position_t{ X, Y, Z }));
+				}
 			}
 
-			ai_work_template<ai_tag_work_farm_fixsoil> work;
-			work.do_ai("Fixing Soil", [&work](entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
-				if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE) {
-					// Do I already have a pick?
-					bool have_tool = false;
-					each<item_t, item_carried_t, claimed_t, item_farming_t>([&e, &have_tool](entity_t &E, item_t &i, item_carried_t &ic, claimed_t &claimed, item_farming_t &farm) {
-						if (ic.carried_by == e.id) have_tool = true;
-					});
-					if (have_tool) {
-						std::cout << "Skipping get hoe - we already have one!\n";
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL;
-						return;
-					}
-					else {
-						// We need to fetch the pick
-						std::size_t tool_id = -1;
-						each<item_t, claimed_t, item_farming_t>([&e, &tool_id](entity_t &E, item_t &i, claimed_t &claimed, item_farming_t &farm) {
-							if (claimed.claimed_by == e.id) tool_id = E.id;
-						});
-						if (tool_id == -1) {
-							std::cout << "Unable to find claimed tool. Bug!\n";
-							work.cancel_work_tag(e);
-							return;
-						}
-
-						h.tool_id = tool_id;
-						auto tool_pos = inventory::get_item_location(tool_id);
-						if (tool_pos) {
-							h.current_path = find_path(pos, *tool_pos);
-							if (h.current_path->success) {
-								h.step = ai_tag_work_farm_fixsoil::fix_steps::FETCH_HOE;
-							}
-							else {
-								work.cancel_work_tag(e);
-								return;
-							}
-						}
-						else {
-							work.cancel_work_tag(e);
-							return;
-						}
-					}
+			h.current_path.reset();
+			bool done = false;
+			auto iterator = clear_targets.begin();
+			while (!done && !h.current_path) {
+				h.current_path = find_path(pos, iterator->second);
+				if (!h.current_path->success) {
+					++iterator;
+					if (iterator == clear_targets.end()) done = true;
 				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FETCH_HOE) {
-					work.follow_path(h, pos, e, [&h]() {
-						// Cancel
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE;
-						return;
-					}, [&h, &e]() {
-						// Success
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET;
-
-						inventory_system::pickup_item(h.tool_id, e.id);
-						return;
-					});
+				else {
+					h.step = ai_tag_work_farm_fixsoil::fix_steps::GOTO_TARGET;
 					return;
 				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL) {
-					if (!inventory::is_item_category_available<item_topsoil_t>()) {
-						// No soil available - bail out
-						work.cancel_work_tag(e);
-						return;
-					}
-					auto soil_id = inventory::find_closest_unclaimed_item_by_category_and_claim_it_immediately<item_topsoil_t>(e.id, pos);
-					h.soil_id = soil_id;
-					if (soil_id == 0) {
-						// No soil available - bail out
-						work.cancel_work_tag(e);
-						return;
-					}
-					auto soil_position = inventory::get_item_location(soil_id);
-					if (!soil_position) {
-						// No soil available - bail out
-						work.cancel_work_tag(e);
-						return;
-					}
-					h.current_path.reset();
-					h.current_path = find_path(pos, *soil_position);
-					if (!h.current_path->success) {
-						// No soil available - bail out
-						work.cancel_work_tag(e);
-						return;
-					}
-					h.step = ai_tag_work_farm_fixsoil::fix_steps::FETCH_SOIL;
-					return;
-				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FETCH_SOIL) {
-					work.follow_path(h, pos, e, [&h]() {
-						// Cancel
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL;
-						return;
-					}, [&h, &e]() {
-						// Success
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET;
+			}
+			if (!h.current_path) {
+				work.cancel_work_tag(e);
+				return;
+			}
+		}
 
-						inventory_system::pickup_item(h.soil_id, e.id);
-						return;
-					});
-					return;
-				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET) {
-					std::map<int, position_t> clear_targets;
-					for (const auto &f : farm_designations->farms) {
-						if (f.second.state == farm_steps::FIX_SOIL) {
-							auto[X, Y, Z] = idxmap(f.first);
-							const float distance = bengine::distance3d(pos.x, pos.y, pos.z, X, Y, Z);
-							clear_targets.insert(std::make_pair(static_cast<int>(distance), position_t{ X, Y, Z }));
-						}
-					}
-
-					h.current_path.reset();
-					bool done = false;
-					auto iterator = clear_targets.begin();
-					while (!done && !h.current_path) {
-						h.current_path = find_path(pos, iterator->second);
-						if (!h.current_path->success) {
-							++iterator;
-							if (iterator == clear_targets.end()) done = true;
-						}
-						else {
-							h.step = ai_tag_work_farm_fixsoil::fix_steps::GOTO_TARGET;
-							return;
-						}
-					}
-					if (!h.current_path) {
-						work.cancel_work_tag(e);
-						return;
-					}
-					return;
-				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::GOTO_TARGET) {
-					work.follow_path(h, pos, e, [&h]() {
-						// Cancel
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET;
-						return;
-					}, [&h, &e]() {
-						// Success
-						h.current_path.reset();
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIX_TARGET;
-						return;
-					});
-					return;
-				}
-				else if (h.step == ai_tag_work_farm_fixsoil::fix_steps::FIX_TARGET) {
-					const int idx = mapidx(pos);
-					auto stats = e.component<game_stats_t>();
-					if (!stats) {
-						std::cout << "Cancelled because of missing stats.\n";
-						work.cancel_work_tag(e);
-						inventory_system::drop_item(h.soil_id, pos.x, pos.y, pos.z);
-						return;
-					}
-
-					auto farm_finder = farm_designations->farms.find(idx);
-					if (farm_finder == farm_designations->farms.end() || farm_finder->second.state != farm_steps::FIX_SOIL) {
-						std::cout << "Bailing out - not relevant anymore!";
-						work.cancel_work_tag(e);
-						inventory_system::drop_item(h.soil_id, pos.x, pos.y, pos.z);
-						return;
-					}
-
-					auto skill_check = skill_roll(e.id, *stats, rng, "Farming", DIFFICULTY_AVERAGE);
-					if (skill_check >= SUCCESS) {
-
-						// Transform the tile into soil
-						auto topsoil_entity = entity(h.soil_id);
-						if (!topsoil_entity) { work.cancel_work_tag(e); return; }
-						auto topsoil_item = topsoil_entity->component<item_t>();
-						if (!topsoil_item) { work.cancel_work_tag(e); return; }
-						region::set_tile_material(idx, topsoil_item->material);
-
-						// Destroy the topsoil item
-						inventory::delete_item(h.soil_id);
-						h.soil_id = 0;
-
-						// Update the farm
-						farm_finder->second.state = farm_steps::PLANT_SEEDS;
-
-						// Update the tile/chunk
-						chunks::mark_chunk_dirty_by_tileidx(idx);
-						
-						// Become idle - done
-						call_home("AI", "Fix Soil");
-						work.cancel_work_tag(e);
-						h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE;
-						return;
-					}
-					else {
-						if (skill_check == CRITICAL_FAIL) damage_system::inflict_damage(damage_system::inflict_damage_message{ e.id, 1, "Farming Accident" });
-						return;
-					}
-				}
-
+		inline void goto_target(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			work.follow_path(h, pos, e, [&h]() {
+				// Cancel
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET;
+				return;
+			}, [&h, &e]() {
+				// Success
+				h.current_path.reset();
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIX_TARGET;
+				return;
 			});
+		}
+
+		inline void fix_target(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			const int idx = mapidx(pos);
+			auto farm_finder = farm_designations->farms.find(idx);
+			if (farm_finder == farm_designations->farms.end() || farm_finder->second.state != farm_steps::FIX_SOIL) {
+				std::cout << "Bailing out - not relevant anymore!";
+				work.cancel_work_tag(e);
+				inventory_system::drop_item(h.soil_id, pos.x, pos.y, pos.z);
+				return;
+			}
+
+
+			if (work::skill_check_or_damage(e, "Farming", DIFFICULTY_AVERAGE, work, 1, "Farming Accident")) {
+				// Transform the tile into soil
+				auto topsoil_entity = entity(h.soil_id);
+				if (!topsoil_entity) { work.cancel_work_tag(e); return; }
+				auto topsoil_item = topsoil_entity->component<item_t>();
+				if (!topsoil_item) { work.cancel_work_tag(e); return; }
+				region::set_tile_material(idx, topsoil_item->material);
+
+				// Destroy the topsoil item
+				inventory::delete_item(h.soil_id);
+				h.soil_id = 0;
+
+				// Update the farm
+				farm_finder->second.state = farm_steps::PLANT_SEEDS;
+
+				// Update the tile/chunk
+				chunks::mark_chunk_dirty_by_tileidx(idx);
+
+				// Become idle - done
+				call_home("AI", "Fix Soil");
+				work.cancel_work_tag(e);
+				h.step = ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE;
+			}
+		}
+
+		void dispatch(entity_t &e, ai_tag_work_farm_fixsoil &h, ai_tag_my_turn_t &t, position_t &pos) {
+			switch (h.step) {
+			case ai_tag_work_farm_fixsoil::fix_steps::FIND_HOE : find_hoe(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::FETCH_HOE: fetch_hoe(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::FIND_SOIL: find_soil(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::FETCH_SOIL: fetch_soil(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::FIND_TARGET: find_target(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::GOTO_TARGET: goto_target(e, h, t, pos); break;
+			case ai_tag_work_farm_fixsoil::fix_steps::FIX_TARGET: fix_target(e, h, t, pos); break;
+			}
+		}
+
+		void run(const double &duration_ms) {
+			work.do_work(jobs_board::evaluate_farm_fixing, dispatch, job_tag);
 		}
 	}
 }
