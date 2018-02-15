7cd4f5d43344bd1d57a78ea9e9d33d138b0bc6dc
Herbert Wolverson <herberticus@gmail.com>
2018-02-15 09:49:08 -0600

    Move config file to save path.

--- src/global_assets/game_config.cpp
+++ src/global_assets/game_config.cpp
@@ -1,5 +1,6 @@
 #include "game_config.hpp"
 #include "../bengine/string_utils.hpp"
+#include "../bengine/filesystem.hpp"
 #include <fstream>
 
 namespace config {
@@ -7,7 +8,13 @@ namespace config {
     config_t game_config;
 
     void read_config() noexcept {
-        std::ifstream f("world_defs/config.txt");
+		const auto config_filename = get_save_path() + "/config.txt";
+		if (!exists(config_filename))
+		{
+			game_config.save();
+		}
+
+        std::ifstream f(config_filename);
         std::string line;
         while (getline(f, line))
         {
@@ -20,20 +27,26 @@ namespace config {
             if (split_line[0] == "tooltip_fadein" && split_line[1] != "yes") game_config.tooltip_fadein = false;
             if (split_line[0] == "tooltip_speed") game_config.tooltip_speed = std::stof(split_line[1]);
             if (split_line[0] == "autosave_minutes") game_config.autosave_minutes = std::stoi(split_line[1]);
-            if (split_line[0] == "fullscreen" && split_line[1]=="1") game_config.fullscreen = true;
-            if (split_line[0] == "gui_ttf") game_config.gui_ttf = split_line[1];
+            if (split_line[0] == "fullscreen" && split_line[1]=="yes") game_config.fullscreen = true;
+			if (split_line[0] == "fullscreen" && split_line[1] == "no") game_config.fullscreen = false;
+			if (split_line[0] == "gui_ttf") game_config.gui_ttf = split_line[1];
             if (split_line[0] == "gui_ttf_size") game_config.gui_ttf_size = std::stoi(split_line[1]);
             if (split_line[0] == "scale_factor") game_config.scale_factor = std::stof(split_line[1]);
             if (split_line[0] == "allow_calling_home" && split_line[1] != "yes") game_config.allow_calling_home = false;
             if (split_line[0] == "online_username") game_config.online_username = split_line[1];
             if (split_line[0] == "show_entity_ids" && split_line[1]!="yes") game_config.show_entity_ids = false;
             if (split_line[0] == "build_tech_trees" && split_line[1]=="yes") game_config.build_tech_trees = true;
-
+			if (split_line[0] == "texture_size") game_config.texture_size = std::stoi(split_line[1]);
         }
+		if (game_config.texture_size == 0 || (game_config.texture_size & (game_config.texture_size - 1)) != 0)
+		{
+			game_config.texture_size = 512; // Restore to default because it isn't a power of 2!
+		}
     }
 
     void config_t::save() const noexcept {
-        std::ofstream f("world_defs/config.txt");
+		const auto config_filename = get_save_path() + "/config.txt";
+        std::ofstream f(config_filename);
         f << "window_width=" << window_width << "\n";
         f << "window_height=" << window_height << "\n";
         f << "game_font=" << game_font << "\n";
@@ -47,9 +60,9 @@ namespace config {
         f << "tooltip_speed=" << tooltip_speed << "\n";
         f << "autosave_minutes=" << autosave_minutes << "\n";
         if (fullscreen) {
-            f << "fullscreen=1\n";
+            f << "fullscreen=yes\n";
         } else {
-            f << "fullscreen=0\n";
+            f << "fullscreen=no\n";
         }
         f << "gui_ttf=" << gui_ttf << "\n";
         f << "gui_ttf_size=" << gui_ttf_size << "\n";
@@ -71,6 +84,7 @@ namespace config {
         } else {
             f << "no\n";
         }
+		f << "texture_size=" << texture_size << "\n";
     }
 
 }
--- src/global_assets/game_config.hpp
+++ src/global_assets/game_config.hpp
@@ -5,22 +5,23 @@
 namespace config {
 
 	struct config_t {
-		int window_width = 800;
-		int window_height = 600;
-		std::string game_font = "8x8";
+		int window_width = 1400;
+		int window_height = 900;
+		std::string game_font = "24x24";
 		std::string gui_font = "8x16";
 		std::string game_font_small = "8x8";
 		bool tooltip_fadein = true;
 		float tooltip_speed = 100.0;
 		int autosave_minutes = 5;
-		bool fullscreen = false;
-		std::string gui_ttf = "neuropol.ttf";
-		int gui_ttf_size = 24;
+		bool fullscreen = true;
+		std::string gui_ttf = "yanonekaffeesatz.ttf";
+		int gui_ttf_size = 18;
 		float scale_factor = 1.0f;
 		bool allow_calling_home = true;
 		std::string online_username = "anonymouse";
 		bool show_entity_ids = true;
 		bool build_tech_trees = false;
+		int texture_size = 512;
 
 		void save() const noexcept;
 	};
