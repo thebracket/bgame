97f86614da414a15293f60fc4ff7b555065b8e22
Herbert Wolverson <herberticus@gmail.com>
2018-02-15 08:25:18 -0600

    Mac build should now fix dylib paths.

--- bundle-mac-alpha.sh
+++ bundle-mac-alpha.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUTLER=~/Library/Application\ Support/itch/bin/butler
+BUTLER="~/Library/Application\ Support/itch/bin/butler"
 DATE=`date +%Y-%m-%d`
 FOLDER=/opt/bgame$DATE
 #echo $FOLDER
@@ -22,12 +22,34 @@ cp /opt/local/lib/libfmt.4.dylib $FOLDER
 cp /opt/local/lib/libz.1.dylib $FOLDER
 cp /usr/lib/libcurl.4.dylib $FOLDER
 cp /opt/local/lib/libGLEW.2.0.0.dylib $FOLDER
-cp /System/Library/Frameworks/OpenGL.framework/Versions/A/OpenGL $FOLDER
-cp /opt/local/lib/libboost_system-mt.dylib
+#cp /System/Library/Frameworks/OpenGL.framework/Versions/A/OpenGL $FOLDER
+cp /opt/local/lib/libboost_system-mt.dylib $FOLDER
 cp /opt/local/lib/libboost_filesystem-mt.dylib $FOLDER
 cp /opt/local/lib/libglfw.3.dylib $FOLDER
 cp /opt/local/lib/liblua.dylib $FOLDER
 cp /opt/local/lib/libomp/libomp.dylib $FOLDER
 cp /usr/lib/libc++.1.dylib $FOLDER
 cp /usr/lib/libSystem.B.dylib $FOLDER
+
+echo "Fixing linkage on dylibs"
+NF=$FOLDER/noxfutura
+install_name_tool -change /opt/local/lib/libfmt.4.dylib @executable_path/libfmt.4.dylib $NF
+install_name_tool -change /opt/local/lib/libz.1.dylib @executable_path/libz.1.dylib $NF
+install_name_tool -change /usr/lib/libcurl.4.dylib @executable_path/libcurl.4.dylib $NF
+install_name_tool -change /opt/local/lib/libGLEW.2.0.0.dylib @executable_path/libGLEW.2.0.0.dylib $NF
+#install_name_tool -change /System/Library/Frameworks/OpenGL.framework/Versions/A/OpenGL @executable_path/OpenGL $NF
+install_name_tool -change /opt/local/lib/libboost_system-mt.dylib @executable_path/libboost_system-mt.dylib $NF
+install_name_tool -change /opt/local/lib/libboost_filesystem-mt.dylib @executable_path/libboost_filesystem-mt.dylib $NF
+install_name_tool -change /opt/local/lib/libglfw.3.dylib @executable_path/libglfw.3.dylib $NF
+install_name_tool -change /opt/local/lib/liblua.dylib @executable_path/liblua.dylib $NF
+install_name_tool -change /opt/local/lib/libomp/libomp.dylib @executable_path/libomp.dylib $NF
+install_name_tool -change /usr/lib/libc++.1.dylib @executable_path/libc++.1.dylib $NF
+install_name_tool -change /usr/lib/libSystem.B.dylib @executable_path/libSystem.B.dylib $NF
+
+echo "Butler time!"
+echo $BUTLER
+eval $BUTLER upgrade
+UPLOAD_COMMAND="$BUTLER push $FOLDER thebracket/nox-futura:mac-pre-alpha"
+echo $UPLOAD_COMMAND
+eval $UPLOAD_COMMAND
 
