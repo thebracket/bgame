2bc7736894f329f3257e21a293281a5bca827c71
Herbert Wolverson <herberticus@gmail.com>
2018-02-15 14:36:54 -0600

    Remove TBN calculation from shader, since we aren't using it.

--- game_assets/chunkshader_fragment.glsl
+++ game_assets/chunkshader_fragment.glsl
@@ -4,7 +4,6 @@ uniform sampler2DArray textureArray;
 
 in vec3 tex_pos;
 in vec3 world_pos;
-in mat3 TBN;
 in vec3 base_normal;
 
 layout (location = 0) out vec3 gPosition;
@@ -21,11 +20,9 @@ void main() {
 
     vec3 norm = texture(textureArray, vec3(tex_pos.x, tex_pos.y, tex_pos.z+1)).rgb;
     norm = normalize(norm);
-    norm = normalize(TBN * norm);
     gNormal = norm;
 
     gAmbientOcclusion.r = texture(textureArray, vec3(tex_pos.x, tex_pos.y, tex_pos.z+2)).r;
     gAmbientOcclusion.g = texture(textureArray, vec3(tex_pos.x, tex_pos.y, tex_pos.z+2)).g;
     gAmbientOcclusion.b = texture(textureArray, vec3(tex_pos.x, tex_pos.y, tex_pos.z+2)).b;
-    //gAlbedo = lightColor;
 }
