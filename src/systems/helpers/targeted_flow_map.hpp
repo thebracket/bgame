d24e2f074740cd46ef82fe358d4ccdc00f6a1439
Herbert Wolverson <herberticus@gmail.com>
2018-02-23 08:53:47 -0600

    Quick code cleanup.

--- src/systems/helpers/targeted_flow_map.hpp
+++ src/systems/helpers/targeted_flow_map.hpp
@@ -4,7 +4,6 @@
 #include "pathfinding.hpp"
 #include "../ai/distance_map_system.hpp"
 #include "../../bengine/geometry.hpp"
-#include <iostream>
 #include <map>
 #include <vector>
 #include <tuple>
@@ -30,10 +29,8 @@ namespace flow_maps {
 			targets.clear();
 			for (const auto &t : new_targets)
 			{
-				const int idx = std::get<0>(t);
+				const auto idx = std::get<0>(t);
 				if (region::flag(idx, tile_flags::CAN_STAND_HERE) && systems::distance_map::reachable_from_cordex.get(idx) < systems::dijkstra::MAX_DIJSTRA_DISTANCE) {
-					auto[x, y, z] = idxmap(idx);
-					std::cout << x << "," << y << "," << z << "\n";
 					targets.emplace_back(t);
 				}
 			}
@@ -46,7 +43,6 @@ namespace flow_maps {
 			{
 				const auto cordex_reachable = systems::distance_map::reachable_from_cordex.get(std::get<0>(t));
 				const auto[x, y, z] = idxmap(std::get<0>(t));
-				std::cout << x << "," << y << "," << z << " = " << cordex_reachable << "\n";
 				const auto range = static_cast<int>(bengine::distance3d(pos.x, pos.y, pos.z, x, y, z));
 				if (cordex_reachable < systems::dijkstra::MAX_DIJSTRA_DISTANCE-1)
 					searcher.insert(std::make_pair(range, t));
@@ -55,7 +51,6 @@ namespace flow_maps {
 			for (const auto search : searcher)
 			{
 				const auto[x, y, z] = idxmap(std::get<0>(search.second));
-				std::cout << "Path to " << x << "," << y << "," << z << "\n";
 				auto path = find_path(pos, position_t{ x,y,z });
 				if (path->success) return path_result_t{ std::move(path), std::get<1>(search.second) };
 			}
