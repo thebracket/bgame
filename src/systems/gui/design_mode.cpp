1ea4e85681eed089927b9514184252631e08b203
Herbert Wolverson <herberticus@gmail.com>
2018-02-25 17:32:51 -0600

    First pass at cleaning up the design UI.

--- src/systems/gui/design_mode.cpp
+++ src/systems/gui/design_mode.cpp
@@ -8,6 +8,7 @@
 #include "../../render_engine/design_render.hpp"
 #include "../../bengine/gl_include.hpp"
 #include <string>
+#include "../../bengine/main_window.hpp"
 
 namespace systems {
 	namespace design_mode {
@@ -16,6 +17,19 @@ namespace systems {
 		const std::string btn_close = std::string(ICON_FA_TIMES) + " Close";
 
 		void run(const double &duration_ms) {
+			if (game_master_mode == PLAY || game_master_mode == DESIGN)
+			{
+				if (glfwGetKey(bengine::main_window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS || glfwGetKey(bengine::main_window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS) {
+					if (is_key_down(GLFW_KEY_T, false)) game_design_mode = CHOPPING;
+					if (is_key_down(GLFW_KEY_B, false)) game_design_mode = BUILDING;
+					if (is_key_down(GLFW_KEY_G, false)) game_design_mode = GUARDPOINTS;
+					if (is_key_down(GLFW_KEY_H, false)) game_design_mode = HARVEST;
+					if (is_key_down(GLFW_KEY_M, false)) game_design_mode = DIGGING;
+					if (is_key_down(GLFW_KEY_S, false)) game_design_mode = STOCKPILES;
+					if (is_key_down(GLFW_KEY_A, false)) game_design_mode = ARCHITECTURE;
+				}
+			}
+
 			if (game_master_mode == PLAY) {
 				if (is_key_down(GLFW_KEY_D, false)) {
 					game_master_mode = DESIGN;
@@ -24,28 +38,8 @@ namespace systems {
 			}
 			else if (game_master_mode == DESIGN) {
 				pause_mode = PAUSED; // Always pause while designing
-				/*if (is_key_down(GLFW_KEY_T, false)) game_design_mode = CHOPPING;
-				if (is_key_down(GLFW_KEY_B, false)) game_design_mode = BUILDING;
-				if (is_key_down(GLFW_KEY_G, false)) game_design_mode = GUARDPOINTS;
-				if (is_key_down(GLFW_KEY_H, false)) game_design_mode = HARVEST;
-				if (is_key_down(GLFW_KEY_M, false)) game_design_mode = DIGGING;
-				if (is_key_down(GLFW_KEY_S, false)) game_design_mode = STOCKPILES;
-				if (is_key_down(GLFW_KEY_A, false)) game_design_mode = ARCHITECTURE;*/
+				
 				if (is_key_down(GLFW_KEY_ESCAPE)) game_master_mode = PLAY;
-
-				ImGui::Begin(win_design.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize + ImGuiWindowFlags_NoCollapse);
-				ImGui::RadioButton("(M)ining", (int *)&game_design_mode, 0); ImGui::SameLine();
-				ImGui::RadioButton("(B)uilding", (int *)&game_design_mode, 1); ImGui::SameLine();
-				ImGui::RadioButton("(T)ree Chopping", (int *)&game_design_mode, 2); ImGui::SameLine();
-				ImGui::RadioButton("(G)uardpoints", (int *)&game_design_mode, 3); ImGui::SameLine();
-				ImGui::RadioButton("(S)tockpiles", (int *)&game_design_mode, 4); ImGui::SameLine();
-				ImGui::RadioButton("(H)arvest", (int *)&game_design_mode, 5); ImGui::SameLine();
-				ImGui::RadioButton("(A)rchitecture", (int *)&game_design_mode, 6);
-
-				if (ImGui::Button(btn_close.c_str())) {
-					game_master_mode = PLAY;
-				}
-				ImGui::End();
 			}
 		}
 	}
--- src/systems/gui/hud_system.cpp
+++ src/systems/gui/hud_system.cpp
@@ -56,28 +56,57 @@ namespace systems {
 			
 		}
 
-        void run(const double &duration_ms) {
-            // Display cash
-			fmt::MemoryWriter cash_ss;
-            cash_ss << " Cash: " << designations->current_cash << " Mcr";
-			fmt::MemoryWriter power_ss;
-            power_ss << " Power: " << designations->current_power << "/" << designations->total_capacity << " ";
-			fmt::MemoryWriter pause_ss;
-            if (pause_mode) {
-                pause_ss << "*PAUSED* (SPACE to unpause, . for single-step) ";
-            }
-            const std::string cash_str = pause_ss.str() + std::string(ICON_FA_CALENDAR) + std::string(" ") + calendar->get_date_time() + std::string(" ") + std::string(ICON_FA_LIGHTBULB_O) + power_ss.str() + std::string(ICON_FA_MONEY) + cash_ss.str();
+		struct design_mode_t
+		{
+			std::string label;
+			std::string shortcut;
+			game_design_mode_t target;
+		};
 
-            // Setup the main menu
-            ImGui::BeginMainMenuBar();
-            if (ImGui::BeginMenu(menu_main.c_str())) {
-                if (ImGui::MenuItem(menu_main_play.c_str())) {
-                    /*game_master_mode = PLAY;
-                    emit_deferred(map_dirty_message{});
-                    emit_deferred(recalculate_mining_message{});
-                    world_changed=true;*/
-                }
-                if (ImGui::BeginMenu(menu_display.c_str())) {
+		static const std::vector<design_mode_t> DESIGN_MODES{
+			design_mode_t{ menu_design_architecture, "^A", ARCHITECTURE },
+			design_mode_t{ menu_design_building, "^B", BUILDING },
+			design_mode_t{ menu_design_guard, "^G", GUARDPOINTS },
+			design_mode_t{ menu_design_harvest, "^H", HARVEST },
+			design_mode_t{ menu_design_mining, "^M", DIGGING },
+			design_mode_t{ menu_design_stockpiles, "^S", STOCKPILES },
+			design_mode_t{ menu_design_tree_cutting, "^T", CHOPPING },
+		};
+
+		static void add_design_elements()
+		{
+			for (const auto &mode : DESIGN_MODES)
+			{
+				if (ImGui::MenuItem(mode.label.c_str(), mode.shortcut.c_str()))
+				{
+					game_master_mode = DESIGN;
+					pause_mode = PAUSED;
+					game_design_mode = mode.target;
+					render::mode_change = true;
+				}
+			}
+		}
+
+		static void normal_menu()
+		{
+			// Display cash
+			fmt::MemoryWriter cash_ss;
+			cash_ss << " Cash: " << designations->current_cash << " Mcr";
+			fmt::MemoryWriter power_ss;
+			power_ss << " Power: " << designations->current_power << "/" << designations->total_capacity << " ";
+			fmt::MemoryWriter pause_ss;
+			if (pause_mode) {
+				pause_ss << "*PAUSED* (SPACE to unpause, . for single-step) ";
+			}
+			const std::string cash_str = pause_ss.str() + std::string(ICON_FA_CALENDAR) + std::string(" ") + calendar->get_date_time() + std::string(" ") + std::string(ICON_FA_LIGHTBULB_O) + power_ss.str() + std::string(ICON_FA_MONEY) + cash_ss.str();
+
+			// Setup the main menu
+			ImGui::BeginMainMenuBar();
+			if (ImGui::BeginMenu(menu_main.c_str())) {
+				if (ImGui::MenuItem(menu_main_play.c_str())) {
+					game_master_mode = PLAY;
+				}
+				if (ImGui::BeginMenu(menu_display.c_str())) {
 					if (!camera->ascii_mode) {
 						if (ImGui::BeginMenu(menu_camera_view.c_str())) {
 							if (ImGui::MenuItem("Top-Down")) change_camera_view(game_camera_mode_t::TOP_DOWN);
@@ -96,100 +125,90 @@ namespace systems {
 						}
 					}
 					if (ImGui::MenuItem(menu_camera_ascii.c_str())) {
-                        camera->ascii_mode = !camera->ascii_mode;
+						camera->ascii_mode = !camera->ascii_mode;
 						bengine::analytics::on_event("game", "renderMode", camera->ascii_mode ? "ASCII" : "3D");
 						render::camera_moved = true;
 						render::models_changed = true;
-                    }
-                    ImGui::EndMenu();
-                }
-                if (ImGui::MenuItem(menu_main_quit.c_str())) {
-                    glfwSetWindowShouldClose(bengine::main_window, true);
-                }
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu(menu_design.c_str())) {
-                if (ImGui::MenuItem(menu_design_mining.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = DIGGING;
-					render::mode_change = true;
-                }
-                if (ImGui::MenuItem(menu_design_building.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = BUILDING;
-					render::mode_change = true;
-                }
-                if (ImGui::MenuItem(menu_design_tree_cutting.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = CHOPPING;
-                }
-                if (ImGui::MenuItem(menu_design_guard.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = GUARDPOINTS;
-					render::mode_change = true;
-                }
-                if (ImGui::MenuItem(menu_design_stockpiles.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = STOCKPILES;
-					render::mode_change = true;
-                }
-                if (ImGui::MenuItem(menu_design_harvest.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = HARVEST;
-					render::mode_change = true;
-                }
-                if (ImGui::MenuItem(menu_design_architecture.c_str())) {
-                    game_master_mode = DESIGN;
-                    pause_mode = PAUSED;
-                    game_design_mode = ARCHITECTURE;
-					render::mode_change = true;
-                }
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu(menu_units.c_str())) {
-                if (ImGui::MenuItem(menu_units_units.c_str())) {
-                    game_master_mode = UNITS;
-                    pause_mode = PAUSED;
-                }
-                if (ImGui::MenuItem(menu_units_workflow.c_str())) {
-                    game_master_mode = WORKFLOW;
-                    pause_mode = PAUSED;
-                }
-                if (ImGui::MenuItem(menu_units_civs.c_str())) {
-                    game_master_mode = CIVS;
-                    pause_mode = PAUSED;
-                }
-                if (ImGui::MenuItem(menu_units_standing_orders.c_str())) {
-                    /*game_master_mode = STANDING_ORDERS;
-                    pause_mode = PAUSED;
-                    emit_deferred(map_dirty_message{});*/
-                }
-				if (ImGui::MenuItem(menu_units_jobcenter.c_str())) {
+					}
+					ImGui::EndMenu();
+				}
+				if (ImGui::MenuItem(menu_main_quit.c_str())) {
+					glfwSetWindowShouldClose(bengine::main_window, true);
+				}
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu(menu_design.c_str())) {
+				add_design_elements();
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu(menu_units.c_str())) {
+				if (ImGui::MenuItem(menu_units_units.c_str(), "U")) {
+					game_master_mode = UNITS;
+					pause_mode = PAUSED;
+				}
+				if (ImGui::MenuItem(menu_units_workflow.c_str(), "W")) {
+					game_master_mode = WORKFLOW;
+					pause_mode = PAUSED;
+				}
+				if (ImGui::MenuItem(menu_units_civs.c_str(), "C")) {
+					game_master_mode = CIVS;
+					pause_mode = PAUSED;
+				}
+				if (ImGui::MenuItem(menu_units_standing_orders.c_str())) {
+					/*game_master_mode = STANDING_ORDERS;
+					pause_mode = PAUSED;
+					emit_deferred(map_dirty_message{});*/
+				}
+				if (ImGui::MenuItem(menu_units_jobcenter.c_str(), "J")) {
 					game_master_mode = JOB_CENTER_MODE;
 					pause_mode = PAUSED;
 				}
-                ImGui::EndMenu();
-            }
+				ImGui::EndMenu();
+			}
 
-            auto status_size = ImGui::CalcTextSize(cash_str.c_str());
-            ImGui::SameLine(ImGui::GetWindowWidth()-(status_size.x + 10));
-            ImGui::Text("%s", cash_str.c_str());
-            ImGui::EndMainMenuBar();
+			const auto status_size = ImGui::CalcTextSize(cash_str.c_str());
+			ImGui::SameLine(ImGui::GetWindowWidth() - (status_size.x + 10));
+			ImGui::Text("%s", cash_str.c_str());
+			ImGui::EndMainMenuBar();
+		}
 
-			// Keyboard shortcuts
-			if (game_master_mode == PLAY) {
-				if (is_key_down(GLFW_KEY_U, false)) game_master_mode = UNITS;
-				if (is_key_down(GLFW_KEY_C, false)) game_master_mode = CIVS;
-				if (is_key_down(GLFW_KEY_W, false)) game_master_mode = WORKFLOW;
-				if (is_key_down(GLFW_KEY_J, false)) game_master_mode = JOB_CENTER_MODE;
-				if (is_key_down(GLFW_KEY_W, false)) game_master_mode = WISHMODE;
-				if (is_key_down(GLFW_KEY_APOSTROPHE)) render::depth_test_render = !render::depth_test_render;
+		static void design_menu()
+		{
+			ImGui::BeginMainMenuBar();
+			ImGui::Text("%s", "Design Mode");
+
+			for (const auto &mode : DESIGN_MODES)
+			{
+				const auto modecat = mode.label + std::string(" (") + mode.shortcut + std::string(")");
+				if (ImGui::BeginMenu(modecat.c_str()))
+				{
+					game_master_mode = DESIGN;
+					pause_mode = PAUSED;
+					game_design_mode = mode.target;
+					render::mode_change = true;
+					ImGui::EndMenu();
+				}
+			}
+
+			ImGui::EndMainMenuBar();
+		}
+
+        void run(const double &duration_ms) {
+			if (game_master_mode != DESIGN) {
+				normal_menu();
+
+				// Keyboard shortcuts
+				if (game_master_mode == PLAY) {
+					if (is_key_down(GLFW_KEY_U, false)) game_master_mode = UNITS;
+					if (is_key_down(GLFW_KEY_C, false)) game_master_mode = CIVS;
+					if (is_key_down(GLFW_KEY_W, false)) game_master_mode = WORKFLOW;
+					if (is_key_down(GLFW_KEY_J, false)) game_master_mode = JOB_CENTER_MODE;
+					if (is_key_down(GLFW_KEY_W, false)) game_master_mode = WISHMODE;
+					if (is_key_down(GLFW_KEY_APOSTROPHE)) render::depth_test_render = !render::depth_test_render;
+				}
+			} else
+			{
+				design_menu();
 			}
 		}
     }
