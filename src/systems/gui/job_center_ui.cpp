4a88f71a903c144e02d73890559623e7081f3cb3
Herbert Wolverson <herberticus@gmail.com>
2018-02-25 11:15:15 -0600

    Job center UI uses the new btabs.

--- src/systems/gui/job_center_ui.cpp
+++ src/systems/gui/job_center_ui.cpp
@@ -1,17 +1,12 @@
 #include "job_center_ui.hpp"
 #include "../../bengine/IconsFontAwesome.h"
 #include "../../bengine/imgui.h"
-#include "../../bengine/imgui_impl_glfw_gl3.h"
 #include "../../components/settler_ai.hpp"
 #include "../../components/name.hpp"
 #include "../../components/game_stats.hpp"
 #include "../../components/species.hpp"
-#include "../../components/grazer_ai.hpp"
 #include "../../components/sentient_ai.hpp"
 #include "../../global_assets/game_mode.hpp"
-#include "../../global_assets/game_camera.hpp"
-#include "../../render_engine/camera.hpp"
-#include "../../render_engine/vox/renderables.hpp"
 #include "../../components/health.hpp"
 #include "../helpers/inventory_assistant.hpp"
 #include "../../components/item_tags/item_digging_t.hpp"
@@ -23,6 +18,7 @@
 #include "../../components/item_tags/item_farming.hpp"
 #include "../../components/farming/designated_hunter.hpp"
 #include "../ai/inventory_system.hpp"
+#include "../../bengine/btabs.hpp"
 
 namespace systems {
 	namespace job_center_ui {
@@ -34,19 +30,22 @@ namespace systems {
 		static const std::string hunting_job_tab = std::string(ICON_FA_BULLSEYE) + " Hunting";
 		const static std::string btn_close = std::string(ICON_FA_TIMES) + " Close";
 
+		static bool miner_headings_first_run = true;
+		static bool lumberjack_headings_first_run = true;
+		static bool farmers_headings_first_run = true;
+		static bool hunters_headings_first_run = true;
+		static std::vector<bengine::table_heading_t> jcui_headings{
+			{ "Settler Name", 150.0f },{ "Profession", -1.0f }, { "Suitability", -1.0f}, {"Options", -1.0f}
+		};
+
 		static inline void render_miners() {
 			using namespace bengine;
 
-			ImGui::Columns(4, "miner_list_grid");
-			ImGui::Separator();
+			bengine::begin_table(miner_headings_first_run, jcui_headings, "miners_list_grid");
 
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Settler Name"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Profession"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Suitability"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Options"); ImGui::NextColumn();
-			ImGui::Separator();
-
-			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+			bool zebra = true;
+			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([&zebra](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+				bengine::zebra_row(zebra);
 				auto miner_designation = e.component<designated_miner_t>();
 				const std::string gender = (species.gender == MALE) ? std::string(ICON_FA_MALE) : std::string(ICON_FA_FEMALE);
 				const std::string dname = name.first_name + std::string(" ") + name.last_name;
@@ -57,19 +56,23 @@ namespace systems {
 					if (sk.first == "Mining") mine_skill = sk.second.skill_level;
 				}
 
+				bengine::begin_zebra_col(zebra);
 				if (!miner_designation) {
 					ImGui::Text("%s %s", gender.c_str(), dname.c_str());
 				}
 				else {
 					ImGui::TextColored(ImVec4{ 1.0f, 1.0f, 0.0f, 1.0f }, "%s %s", gender.c_str(), dname.c_str());
 				}
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra); 
 				ImGui::Text("%s", profession.c_str());
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
 
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("Str: %d, Dex: %d, HP: %d/%d, Skill: %d", stats.strength, stats.dexterity, h.current_hitpoints, h.max_hitpoints, mine_skill);
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
 
+				bengine::begin_zebra_col(zebra);
 				if (!miner_designation && e.component<designated_lumberjack_t>() == nullptr && e.component<designated_farmer_t>() == nullptr && e.component<designated_hunter_t>() == nullptr) {
 					if (inventory::is_item_category_available<item_digging_t>()) {
 						const std::string btn_makeminer = std::string(ICON_FA_DIAMOND) + std::string(" Make Miner##") + std::to_string(e.id);
@@ -108,24 +111,19 @@ namespace systems {
 					}
 				}
 
-				ImGui::NextColumn();
-				ImGui::Separator();
+				bengine::end_zebra_col();
 			});
+			bengine::end_table();
 		}
 
 		static inline void render_lumberjacks() {
 			using namespace bengine;
 
-			ImGui::Columns(4, "lumberjack_list_grid");
-			ImGui::Separator();
+			bengine::begin_table(lumberjack_headings_first_run, jcui_headings, "lumberjack_list_grid");
 
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Settler Name"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Profession"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Suitability"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Options"); ImGui::NextColumn();
-			ImGui::Separator();
-
-			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+			bool zebra = true;
+			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([&zebra](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+				bengine::zebra_row(zebra);
 				auto lj_designation = e.component<designated_lumberjack_t>();
 				const std::string gender = (species.gender == MALE) ? std::string(ICON_FA_MALE) : std::string(ICON_FA_FEMALE);
 				const std::string dname = name.first_name + std::string(" ") + name.last_name;
@@ -136,19 +134,23 @@ namespace systems {
 					if (sk.first == "Lumberjacking") mine_skill = sk.second.skill_level;
 				}
 
+				bengine::begin_zebra_col(zebra);
 				if (!lj_designation) {
 					ImGui::Text("%s %s", gender.c_str(), dname.c_str());
 				}
 				else {
 					ImGui::TextColored(ImVec4{ 1.0f, 1.0f, 0.0f, 1.0f }, "%s %s", gender.c_str(), dname.c_str());
 				}
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("%s", profession.c_str());
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
 
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("Str: %d, Dex: %d, HP: %d/%d, Skill: %d", stats.strength, stats.dexterity, h.current_hitpoints, h.max_hitpoints, mine_skill);
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
 
+				bengine::begin_zebra_col(zebra);
 				if (!lj_designation && e.component<designated_miner_t>()==nullptr && e.component<designated_farmer_t>() == nullptr && e.component<designated_hunter_t>() == nullptr) {
 					if (inventory::is_item_category_available<item_chopping_t>()) {
 						const std::string btn_makeminer = std::string(ICON_FA_TREE) + std::string(" Make Lumberjack##") + std::to_string(e.id);
@@ -187,24 +189,18 @@ namespace systems {
 					}
 				}
 
-				ImGui::NextColumn();
-				ImGui::Separator();
+				bengine::end_zebra_col();
 			});
 		}
 
 		static inline void render_farmers() {
 			using namespace bengine;
 
-			ImGui::Columns(4, "farmer_list_grid");
-			ImGui::Separator();
+			bengine::begin_table(farmers_headings_first_run, jcui_headings, "farmers_list_grid");
 
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Settler Name"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Profession"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Suitability"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Options"); ImGui::NextColumn();
-			ImGui::Separator();
-
-			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+			bool zebra = true;
+			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([&zebra](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+				bengine::zebra_row(zebra);
 				auto farm_designation = e.component<designated_farmer_t>();
 				const std::string gender = (species.gender == MALE) ? std::string(ICON_FA_MALE) : std::string(ICON_FA_FEMALE);
 				const std::string dname = name.first_name + std::string(" ") + name.last_name;
@@ -215,19 +211,21 @@ namespace systems {
 					if (sk.first == "Farming") mine_skill = sk.second.skill_level;
 				}
 
+				bengine::begin_zebra_col(zebra);
 				if (!farm_designation) {
 					ImGui::Text("%s %s", gender.c_str(), dname.c_str());
 				}
 				else {
 					ImGui::TextColored(ImVec4{ 1.0f, 1.0f, 0.0f, 1.0f }, "%s %s", gender.c_str(), dname.c_str());
 				}
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("%s", profession.c_str());
-				ImGui::NextColumn();
-
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("Dex: %d, Wis: %d, HP: %d/%d, Skill: %d", stats.dexterity, stats.wisdom, h.current_hitpoints, h.max_hitpoints, mine_skill);
-				ImGui::NextColumn();
-
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				if (!farm_designation && e.component<designated_miner_t>() == nullptr && e.component<designated_lumberjack_t>() == nullptr && e.component<designated_hunter_t>() == nullptr) {
 					if (inventory::is_item_category_available<item_farming_t>()) {
 						const std::string btn_makeminer = std::string(ICON_FA_LEAF) + std::string(" Make Farmer##") + std::to_string(e.id);
@@ -266,8 +264,7 @@ namespace systems {
 					}
 				}
 
-				ImGui::NextColumn();
-				ImGui::Separator();
+				bengine::end_zebra_col();
 			});
 		}
 
@@ -275,16 +272,11 @@ namespace systems {
 		{
 			using namespace bengine;
 
-			ImGui::Columns(4, "hunter_list_grid");
-			ImGui::Separator();
+			bengine::begin_table(hunters_headings_first_run, jcui_headings, "hunters_list_grid");
 
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Settler Name"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Profession"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Suitability"); ImGui::NextColumn();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%s", "Options"); ImGui::NextColumn();
-			ImGui::Separator();
-
-			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+			bool zebra = true;
+			each<settler_ai_t, name_t, game_stats_t, species_t, health_t>([&zebra](entity_t &e, settler_ai_t &ai, name_t &name, game_stats_t &stats, species_t &species, health_t &h) {
+				bengine::zebra_row(zebra);
 				auto hunt_designation = e.component<designated_hunter_t>();
 				const std::string gender = (species.gender == MALE) ? std::string(ICON_FA_MALE) : std::string(ICON_FA_FEMALE);
 				const std::string dname = name.first_name + std::string(" ") + name.last_name;
@@ -295,19 +287,21 @@ namespace systems {
 					if (sk.first == "Farming") mine_skill = sk.second.skill_level;
 				}
 
+				bengine::begin_zebra_col(zebra);
 				if (!hunt_designation) {
 					ImGui::Text("%s %s", gender.c_str(), dname.c_str());
 				}
 				else {
 					ImGui::TextColored(ImVec4{ 1.0f, 1.0f, 0.0f, 1.0f }, "%s %s", gender.c_str(), dname.c_str());
 				}
-				ImGui::NextColumn();
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("%s", profession.c_str());
-				ImGui::NextColumn();
-
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				ImGui::Text("Dex: %d, Wis: %d, HP: %d/%d, Skill: %d", stats.dexterity, stats.wisdom, h.current_hitpoints, h.max_hitpoints, mine_skill);
-				ImGui::NextColumn();
-
+				bengine::end_zebra_col();
+				bengine::begin_zebra_col(zebra);
 				if (!hunt_designation && e.component<designated_miner_t>() == nullptr && e.component<designated_lumberjack_t>() == nullptr && e.component<designated_hunter_t>() == nullptr) {
 					const std::string btn_makeminer = std::string(ICON_FA_BULLSEYE) + std::string(" Make Hunter##") + std::to_string(e.id);
 					if (ImGui::Button(btn_makeminer.c_str())) {
@@ -325,36 +319,30 @@ namespace systems {
 					}
 				}
 
-				ImGui::NextColumn();
-				ImGui::Separator();
+				bengine::end_zebra_col();
 			});
 		}
 
+		static bengine::btabs_t jc_tabs{
+			{
+				bengine::btab_t{ mining_job_tab, render_miners },
+				bengine::btab_t{ lumberjacking_job_tab, render_lumberjacks },
+				bengine::btab_t{ farming_job_tab, render_farmers },
+				bengine::btab_t{ hunting_job_tab, render_hunters },
+			}
+		};
+
+		static bool show_window = true;
+
 		void run(const double &duration_ms) {
-			ImGui::Begin(win_job_center.c_str());
-
-			ImGui::SameLine();
-			if (ImGui::Button(btn_close.c_str())) {
-				game_master_mode = PLAY;
-			}
-
-			//ImGui::BeginTabBar("##units#jobs_bar");
-			//ImGui::DrawTabsBackground();
-
-			if (ImGui::Button(mining_job_tab.c_str())) {
-				render_miners();
-			}
-			if (ImGui::Button(lumberjacking_job_tab.c_str())) {
-				render_lumberjacks();
-			}
-			if (ImGui::Button(farming_job_tab.c_str())) {
-				render_farmers();
-			}
-			if (ImGui::Button(hunting_job_tab.c_str())) {
-				render_hunters();
-			}
-			//ImGui::EndTabBar();
+			bengine::begin_info_window(win_job_center, &show_window);
+			bengine::render_btab_bar(jc_tabs);
 			ImGui::End();
+
+			if (!show_window) {
+				game_master_mode = PLAY;
+				show_window = true;
+			}
 		}
 	}
 }
