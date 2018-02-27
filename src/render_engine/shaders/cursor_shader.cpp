2fc88df59c77f945b39c6db622255c45a7f9c221
Herbert Wolverson <herberticus@gmail.com>
2018-02-27 11:51:50 -0600

    Rename cursor shader for more consistency.

--- src/render_engine/shaders/cursor_shader.cpp
+++ src/render_engine/shaders/cursor_shader.cpp
@@ -1,7 +1,7 @@
 #include "cursor_shader.hpp"
 
 cursor_shader_t::cursor_shader_t() {
-	load_shader("game_assets/shaders45/highlight_vertex.glsl", "game_assets/shaders45/highlight_fragment.glsl");
+	load_shader("game_assets/shaders45/cursor_vertex.glsl", "game_assets/shaders45/cursor_fragment.glsl");
 
 	view_matrix = get_uniform("view_matrix");
 	projection_matrix = get_uniform("projection_matrix");
