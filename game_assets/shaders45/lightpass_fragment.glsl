2f64e53cf13c836ea5caebaf8046a4370be73925
Herbert Wolverson <herberticus@gmail.com>
2018-03-05 11:45:14 -0600

    Suitably darken ambient lighting ready for next pass and cleanup.

--- game_assets/shaders45/lightpass_fragment.glsl
+++ game_assets/shaders45/lightpass_fragment.glsl
@@ -67,15 +67,12 @@ void main()
     }
     ambient_color *= moon_color;
 
-    vec3 dir = normalize(camera_position);
-    float lambert = max(dot(normal, dir), 0.0);
-
     float y_difference = 1.0 - ((camera_position.y - position.y) * 0.1);
 
     // Final color
     vec3 ambient = ambient_color * ambient_occlusion;
     FragColor = ambient; // Remove Lo because we're just doing ambient
     FragColor *= SSAO;
-    FragColor *= lambert;
     FragColor *= y_difference;
+    FragColor *= 0.1;
 }
